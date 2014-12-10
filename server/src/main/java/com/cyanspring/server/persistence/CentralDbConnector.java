afda83ed0ac9d91cac858cfb2332b8ca56a0f6e9
phoenixsu <phoenixsu@moneydj.com>
2014-12-10 20:20:30 +0800

    add salt

--- server/src/main/java/com/cyanspring/server/persistence/CentralDbConnector.java
+++ server/src/main/java/com/cyanspring/server/persistence/CentralDbConnector.java
@@ -1,5 +1,6 @@
 package com.cyanspring.server.persistence;
 
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -27,9 +28,9 @@ public class CentralDbConnector {
 	private static SimpleDateFormat sdf = new SimpleDateFormat(
 			"yyyy-MM-dd HH:mm:ss");
 	private static CentralDbConnector inst = null;
-	private static String insertUser = "INSERT INTO AUTH(`USERID`, `USERNAME`, `PASSWORD`, `EMAIL`, `PHONE`, `CREATED`, `USERTYPE`) VALUES('%s', '%s', md5('%s'), '%s', '%s', '%s', %d)";
+	private static String insertUser = "INSERT INTO AUTH(`USERID`, `USERNAME`, `PASSWORD`, `SALT`, `EMAIL`, `PHONE`, `CREATED`, `USERTYPE`) VALUES('%s', '%s', md5('%s'), '%s', '%s', '%s', '%s', %d)";
 	private static String isUserExist = "SELECT COUNT(*) FROM AUTH WHERE `USERID` = '%s'";
-	private static String verifyUserPassword = "SELECT COUNT(*) FROM AUTH WHERE `USERID` ='%s' AND `PASSWORD` = md5('%s')";
+	private static String getUserPasswordSalt = "SELECT `PASSWORD`, `SALT` FROM AUTH WHERE `USERID` = '%s'";
 	private static String openSQL = "jdbc:mysql://%s:%d/%s?useUnicode=true";
 
 	private static final Logger log = LoggerFactory
@@ -100,9 +101,9 @@ public class CentralDbConnector {
 	}
 
 	protected String getInsertUserSQL(String userId, String userName,
-			String password, String email, String phone, Date created,
+			String password, String salt, String email, String phone, Date created,
 			UserType userType) {
-		return String.format(insertUser, userId, userName, password, email,
+		return String.format(insertUser, userId, userName, password+salt, salt, email,
 				phone, sdf.format(created), userType.getCode());
 	}
 
@@ -128,8 +129,9 @@ public class CentralDbConnector {
 
 		boolean bIsSuccess = false;
 		Date now = Default.getCalendar().getTime();
-		String sUserSQL = getInsertUserSQL(userId, userName, password, email,
-				phone, now, userType);
+		String salt = getRandomSalt(10);
+		
+		String sUserSQL = getInsertUserSQL(userId, userName, password, salt, email, phone, now, userType);
 		Statement stmt = null;
 
 		try {
@@ -193,18 +195,25 @@ public class CentralDbConnector {
 		if (!checkConnected())
 			return false;
 
-		String sQuery = String.format(verifyUserPassword, sUser, sPassword);
+		String sQuery = String.format(getUserPasswordSalt, sUser);
 		Statement stmt = null;
-		int nCount = 0;
+
+		String md5Password = null;
+		String salt = null;
 
 		try {
 			stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sQuery);
 
 			if (rs.next())
-				nCount = rs.getInt("COUNT(*)");
+			{
+				md5Password = rs.getString("PASSWORD");
+				salt = rs.getString("SALT");
+			}
 
-			if (nCount > 0)
+			String fullPassword = sPassword + salt;
+			
+			if(md5Password.equals(md5(fullPassword)))
 				return true;
 
 		} catch (SQLException e) {
@@ -260,16 +269,70 @@ public class CentralDbConnector {
 	public void setDatabase(String database) {
 		this.database = database;
 	}
+	
+	 public String md5(String str) 
+	 {
+	    String md5 = null;
+	    
+	    try 
+	    {
+	      MessageDigest md=MessageDigest.getInstance("MD5");
+	      byte[] barr=md.digest(str.getBytes());
+	      StringBuffer sb=new StringBuffer();
+	      
+	      for (int i=0; i < barr.length; i++) 
+	    	  sb.append(byte2Hex(barr[i]));
+	      
+	      return sb.toString();
+	    }
+	    catch(Exception e) 
+	    {
+	    	log.error("create md5 error", e);
+	    }
+	    return md5;
+	 }
+	 
+	 public String byte2Hex(byte b) 
+	 {
+	    String[] h={"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"};
+	    int i=b;
+	    
+	    if (i < 0) 
+	    	{i += 256;}
+	    
+	    return h[i/16] + h[i%16];
+	 }
+		  
+	 public String getRandomSalt(int nLength)
+	 {
+		  int[] word = new int[nLength];
+		  int mod;
+		  for(int i = 0; i < nLength; i++)
+		  {
+			  mod = (int)((Math.random()*7)%3);
+			  if(mod ==1)
+				  word[i]=(int)((Math.random()*10) + 48);
+			  else if(mod ==2)
+				  word[i] = (char)((Math.random()*26) + 65);
+			  else
+				  word[i] = (char)((Math.random()*26) + 97);
+		  }
+		  
+		  StringBuilder sKey = new StringBuilder();
+		  for(int j=0 ; j<nLength ; j++)
+			  sKey.append((char)word[j]);
+		  
+		  return sKey.toString();
+	 }
 
 	public static void main(String[] argv) {
 		CentralDbConnector conn = new CentralDbConnector();
 		boolean bConnect = conn.connect("125.227.191.247", 3306, "tqt001",
 				"tqt001", "LTS");
 		if (bConnect) {
-			// conn.registerUser("test1", "TestUser1", "test1",
-			// "test1@test.com", "+886-12345678", UserType.NORMAL);
-			// boolean bExist = conn.isUserExist("test1");
-			boolean bLogin = conn.userLogin("test1", "xxx");
+			//conn.registerUser("test1", "TestUser1", "test1", "test1@test.com", "+886-12345678", UserType.NORMAL);
+			//boolean bExist = conn.isUserExist("test1");
+			boolean bLogin = conn.userLogin("test1", "test1");
 			System.out.println(bLogin);
 		}
 	}
