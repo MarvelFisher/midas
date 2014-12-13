49f5cd668431a4f4a741d51a1e5619549961dc54
dennis <dennis.chen@hkfdt.com>
2014-12-13 18:42:06 +0800

    wait for 5 sec after kill before restart

--- server/run.sh
+++ server/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 jps|grep cyanspring-server-2.56.jar|cut -d ' ' -f1|xargs kill -9
-
+sleep 5
 #ulimit -n 1024000
 
 #JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=$ip -Dcom.sun.management.jmxremote.port=1234 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
