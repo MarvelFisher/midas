1207ec37c578592e0d082eec4511821ef6fe7b07
elvis <elvis.wu@hkfdt.com>
2015-02-26 14:55:46 +0800

    Change project from fu to fc

--- server/fcrun.sh
+++ server/fcrun.sh
@@ -2,7 +2,7 @@
 
 # fix Apps could not login between LTS restart - AppServer restart
 WORKDIR=$PWD
-cd ../appServer.fu
+cd ../appServer.fc
 ./run.sh stop
 cd $WORKDIR
 
@@ -19,7 +19,7 @@ export LD_LIBRARY_PATH=$WORKDIR/windlib:$LD_LIBRARY_PATH
 #JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=$ip -Dcom.sun.management.jmxremote.port=1234 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -Xloggc:log/gc`date +%Y%m%d_%H%M%S`.log -server -d64 -Xmx3g -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=0 -XX:MaxDirectMemorySize=1G -XX:AutoBoxCacheMax=3000000"
 echo "Starting LTS "`date` | tee -a ./log/console.log
-java ${JAVA_OPTS} -Duser.timezone=GMT+8 -jar jars/cyanspring-server-2.56.jar conf/fuserver.xml >> ./log/console.log &
+java ${JAVA_OPTS} -Duser.timezone=GMT+8 -jar jars/cyanspring-server-2.56.jar conf/fcserver.xml >> ./log/console.log &
 echo $! > LTS_PID
 
 sleep 30
@@ -27,5 +27,5 @@ sleep 30
 
 sleep 60
 echo "Starting AppServer "`date` | tee -a ./log/console.log
-cd ../appServer.fu
+cd ../appServer.fc
 ./run.sh start
