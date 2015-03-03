052daf08cc343f9dbad4d5706cd08facf248af32
steve <steve@hkfdt.com>
2015-03-03 14:50:23 +0800

    CentralDbReadyEvent change ITickTable to AbstractTickTable

--- common/src/main/java/com/cyanspring/common/event/info/CentralDbReadyEvent.java
+++ common/src/main/java/com/cyanspring/common/event/info/CentralDbReadyEvent.java
@@ -3,20 +3,21 @@ package com.cyanspring.common.event.info;
 import java.util.Map;
 
 import com.cyanspring.common.event.RemoteAsyncEvent;
+import com.cyanspring.common.staticdata.AbstractTickTable;
 import com.cyanspring.common.staticdata.ITickTable;
 
 @SuppressWarnings("serial")
 public class CentralDbReadyEvent  extends RemoteAsyncEvent
 {
-	private Map<String, ITickTable> tickTableList = null;
+	private Map<String, AbstractTickTable> tickTableList = null;
 	public CentralDbReadyEvent(String key, String receiver) 
 	{
 		super(key, receiver);
 	}
-	public Map<String, ITickTable> getTickTableList() {
+	public Map<String, AbstractTickTable> getTickTableList() {
 		return tickTableList;
 	}
-	public void setTickTableList(Map<String, ITickTable> tickTableList) {
+	public void setTickTableList(Map<String, AbstractTickTable> tickTableList) {
 		this.tickTableList = tickTableList;
 	}
 }
