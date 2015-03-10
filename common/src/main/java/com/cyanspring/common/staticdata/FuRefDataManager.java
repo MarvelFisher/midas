0490b636e89db57ae84aafabd828dcb5fd37a507
elvis <elvis.wu@hkfdt.com>
2015-03-10 15:15:35 +0800

    Add new interface IFuRefDataManager and relative function

--- common/src/main/java/com/cyanspring/common/staticdata/FuRefDataManager.java
+++ common/src/main/java/com/cyanspring/common/staticdata/FuRefDataManager.java
@@ -16,15 +16,17 @@ import com.cyanspring.common.Clock;
 import com.cyanspring.common.Default;
 import com.cyanspring.common.IPlugin;
 import com.cyanspring.common.staticdata.fu.IFStrategy;
+import com.cyanspring.common.staticdata.fu.IFuRefDataManager;
 import com.cyanspring.common.staticdata.fu.RefDataStrategy;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
-public class FuRefDataManager implements IPlugin, IRefDataManager{
+public class FuRefDataManager implements IPlugin, IRefDataManager, IFuRefDataManager{
 	private static final Logger log = LoggerFactory
 			.getLogger(FuRefDataManager.class);
 	String refDataFile;	
-	Map<String, RefData> map = new HashMap<String, RefData>();
+	Map<String, RefData> refSymbolMap;
+	Map<String, RefData> symbolMap;
 	private String market = Default.getMarket();
 	private XStream xstream = new XStream(new DomDriver());
 	private Map<String,RefDataStrategy> stragetyMap = new HashMap<>();
@@ -33,6 +35,8 @@ public class FuRefDataManager implements IPlugin, IRefDataManager{
 	@Override
 	public void init() throws Exception {
 		log.info("initialising with " + refDataFile);
+		refSymbolMap = new HashMap<String, RefData>();
+		symbolMap = new HashMap<String, RefData>();
 		File file = new File(refDataFile);
 		List<RefData> list;
 		if (file.exists()) {
@@ -53,20 +57,21 @@ public class FuRefDataManager implements IPlugin, IRefDataManager{
 			}
 			stragety.init(cal);
 			stragety.setExchangeRefData(refData);
-			map.put(refData.getSymbol(), refData);
+			refSymbolMap.put(refData.getRefSymbol(), refData); //key = Ref Symbol
+			symbolMap.put(refData.getSymbol(), refData); //key = Symbol
 		}		
-		saveRefDataToFile(refDataFile, new ArrayList<RefData>(map.values()));
+		saveRefDataToFile(refDataFile, new ArrayList<RefData>(refSymbolMap.values()));
 	}
 
 	@Override
 	public void uninit() {
 		log.info("uninitialising");
-		map.clear();
+		refSymbolMap.clear();
 	}
 	
 	@Override
 	public RefData getRefData(String symbol) {
-		return map.get(symbol);
+		return refSymbolMap.get(symbol);
 	}
 
 	@Override
@@ -76,7 +81,7 @@ public class FuRefDataManager implements IPlugin, IRefDataManager{
 
 	@Override
 	public List<RefData> getRefDataList() {
-		return new ArrayList<RefData>(map.values());
+		return new ArrayList<RefData>(refSymbolMap.values());
 	}
 
 	@Override
@@ -99,5 +104,15 @@ public class FuRefDataManager implements IPlugin, IRefDataManager{
 		} catch (IOException e) {
 			log.error(e.getMessage(), e);
 		}	
+	}
+
+	@Override
+	public RefData getRefDataBySymbol(String symbol){
+		return symbolMap.get(symbol);
+	}
+
+	@Override
+	public RefData getRefDataByRefSymbol(String refSymbol) {
+		return refSymbolMap.get(refSymbol);
 	}
 }
