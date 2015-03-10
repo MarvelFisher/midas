89b25412069dc40c88ec79af5b014256d6054b37
elvis <elvis.wu@hkfdt.com>
2015-03-10 17:41:23 +0800

    Merge IFuRefDataManager to IRefDataManager

--- common/src/main/java/com/cyanspring/common/staticdata/fu/IFuRefDataManager.java
+++
@@ -1,9 +0,0 @@
-package com.cyanspring.common.staticdata.fu;
-
-import com.cyanspring.common.staticdata.RefData;
-
-public interface IFuRefDataManager {
-	public void init() throws Exception;
-	public RefData getRefDataByRefSymbol(String refSymbol);
-	public RefData getRefDataBySymbol(String symbol);
-}
