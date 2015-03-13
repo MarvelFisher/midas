5df70a7fca8c0aefa9ec1bb9f1cc0c99dde79183
steve <steve@hkfdt.com>
2015-03-13 23:03:24 +0800

    CDP fix RefSymbolInfo

--- common/src/main/java/com/cyanspring/common/info/FXRefSymbolInfo.java
+++ common/src/main/java/com/cyanspring/common/info/FXRefSymbolInfo.java
@@ -5,66 +5,12 @@ import java.util.Collections;
 import java.util.List;
 
 import com.cyanspring.common.marketdata.SymbolInfo;
-import com.cyanspring.common.staticdata.RefData;
 
-public class FXRefSymbolInfo implements IRefSymbolInfo
+public class FXRefSymbolInfo extends IRefSymbolInfo
 {
-	private List<SymbolInfo> refSymbolInfo = new ArrayList<SymbolInfo>();
-	private String serverMarket;
 	public FXRefSymbolInfo(String serverMarket)
 	{
-		this.serverMarket = serverMarket;
-	}
-
-	@Override
-	public void reset() 
-	{
-		refSymbolInfo.clear();
-	}
-	@Override
-	public void setByRefData(List<RefData> refdataList) 
-	{
-		SymbolInfo symbolinfo = null;
-		String strTmp = null;
-		int index;
-		for (RefData refdata : refdataList)
-		{
-			if (refdata.getExchange() == null || refdata.getSymbol() == null) continue;
-			
-			symbolinfo = new SymbolInfo(serverMarket, refdata.getSymbol());
-			symbolinfo.setExchange(refdata.getExchange());
-			symbolinfo.setWindCode(null);
-			symbolinfo.setHint(refdata.getRefSymbol());
-			strTmp = (refdata.getENDisplayName() == null) ? refdata.getSymbol() : refdata.getENDisplayName();
-			symbolinfo.setEnName(strTmp);
-			symbolinfo.setCnName((refdata.getCNDisplayName() == null) ? strTmp : refdata.getCNDisplayName());
-			symbolinfo.setTwName((refdata.getTWDisplayName() == null) ? strTmp : refdata.getTWDisplayName());
-			symbolinfo.setJpName(strTmp);
-			symbolinfo.setKrName(strTmp);
-			symbolinfo.setEsName(strTmp);
-			symbolinfo.setLotSize(refdata.getLotSize());
-			symbolinfo.setTickTable(refdata.getTickTable());
-			symbolinfo.setCNTradingUnit(refdata.getCNTradingUnit());
-			symbolinfo.setENTradingUnit(refdata.getENTradingUnit());
-			symbolinfo.setTWTradingUnit(refdata.getTWTradingUnit());
-			symbolinfo.setSettlementDate(refdata.getSettlementDate());
-			symbolinfo.setCommissionFee(refdata.getCommissionFee());
-			symbolinfo.setNumeratorDP(refdata.getNumberatorDp());
-			symbolinfo.setDecimalPoint(refdata.getDeciamlPoint());
-			symbolinfo.setMinimalCF(refdata.getMinimalCommissionFee());
-			symbolinfo.setPricePerUnit(refdata.getPricePerUnit());
-			symbolinfo.setMaximalLot(refdata.getMaximalLot());
-			symbolinfo.setStrategy(refdata.getStrategy());
-			symbolinfo.setMarginRate(refdata.getMarginRate());
-			symbolinfo.setCategory(refdata.getCategory());
-			symbolinfo.setDenominator(refdata.getDenominator());
-			symbolinfo.setTradable(refdata.getTradable());
-			index = Collections.binarySearch(refSymbolInfo, symbolinfo);
-			if (index < 0)
-			{
-				refSymbolInfo.add(~index, symbolinfo);
-			}
-		}
+		super(serverMarket);
 	}
 	@Override
 	public List<SymbolInfo> getBySymbolStrings(List<String> symbolList) 
@@ -96,37 +42,5 @@ public class FXRefSymbolInfo implements IRefSymbolInfo
 			}
 		}
 		return infoList;
-	}
-	@Override
-	public List<SymbolInfo> getAllSymbolInfo(String market) 
-	{
-		ArrayList<SymbolInfo> infoList = new ArrayList<SymbolInfo>();
-		for (SymbolInfo symbolinfo : refSymbolInfo)
-		{
-			if (symbolinfo.getMarket().equals(market))
-			{
-				infoList.add(symbolinfo);
-			}
-		}
-		return infoList;
-	}
-
-	@Override
-	public int at(SymbolInfo symbolinfo) 
-	{
-		int index = Collections.binarySearch(refSymbolInfo, symbolinfo);
-		return index;
-	}
-
-	@Override
-	public SymbolInfo get(int index) {
-		if (index >= 0)
-		{
-			return refSymbolInfo.get(index);
-		}
-		else
-		{
-			return null;
-		}
 	}
 }
--- common/src/main/java/com/cyanspring/common/info/IRefSymbolInfo.java
+++ common/src/main/java/com/cyanspring/common/info/IRefSymbolInfo.java
@@ -1,16 +1,95 @@
 package com.cyanspring.common.info;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.cyanspring.common.marketdata.SymbolInfo;
 import com.cyanspring.common.staticdata.RefData;
 
-public interface IRefSymbolInfo {
-	public void reset();
-	public void setByRefData(List<RefData> refdataList);
-	public List<SymbolInfo> getBySymbolStrings(List<String> symbolList);
-	public List<SymbolInfo> getBySymbolInfos(List<SymbolInfo> inputInfoList);
-	public List<SymbolInfo> getAllSymbolInfo(String market);
-	public int at(SymbolInfo symbolinfo);
-	public SymbolInfo get(int index);
+public abstract class IRefSymbolInfo {
+	protected List<SymbolInfo> refSymbolInfo = new ArrayList<SymbolInfo>();
+	protected String serverMarket;
+	
+	public IRefSymbolInfo(String serverMarket)
+	{
+		this.serverMarket = serverMarket;
+	}
+	public void reset()
+	{
+		refSymbolInfo.clear();
+	}
+	public void setByRefData(List<RefData> refdataList)
+	{
+		SymbolInfo symbolinfo = null;
+		String strTmp = null;
+		int index;
+		for (RefData refdata : refdataList)
+		{
+			if (refdata.getExchange() == null || refdata.getSymbol() == null) continue;
+			
+			symbolinfo = new SymbolInfo(serverMarket, refdata.getSymbol());
+			symbolinfo.setExchange(refdata.getExchange());
+			symbolinfo.setWindCode(null);
+			symbolinfo.setHint(refdata.getRefSymbol());
+			strTmp = (refdata.getENDisplayName() == null) ? refdata.getSymbol() : refdata.getENDisplayName();
+			symbolinfo.setEnName(strTmp);
+			symbolinfo.setCnName((refdata.getCNDisplayName() == null) ? strTmp : refdata.getCNDisplayName());
+			symbolinfo.setTwName((refdata.getTWDisplayName() == null) ? strTmp : refdata.getTWDisplayName());
+			symbolinfo.setJpName(strTmp);
+			symbolinfo.setKrName(strTmp);
+			symbolinfo.setEsName(strTmp);
+			symbolinfo.setLotSize(refdata.getLotSize());
+			symbolinfo.setTickTable(refdata.getTickTable());
+			symbolinfo.setCNTradingUnit(refdata.getCNTradingUnit());
+			symbolinfo.setENTradingUnit(refdata.getENTradingUnit());
+			symbolinfo.setTWTradingUnit(refdata.getTWTradingUnit());
+			symbolinfo.setSettlementDate(refdata.getSettlementDate());
+			symbolinfo.setCommissionFee(refdata.getCommissionFee());
+			symbolinfo.setNumeratorDP(refdata.getNumberatorDp());
+			symbolinfo.setDecimalPoint(refdata.getDeciamlPoint());
+			symbolinfo.setMinimalCF(refdata.getMinimalCommissionFee());
+			symbolinfo.setPricePerUnit(refdata.getPricePerUnit());
+			symbolinfo.setMaximalLot(refdata.getMaximalLot());
+			symbolinfo.setStrategy(refdata.getStrategy());
+			symbolinfo.setMarginRate(refdata.getMarginRate());
+			symbolinfo.setCategory(refdata.getCategory());
+			symbolinfo.setDenominator(refdata.getDenominator());
+			symbolinfo.setTradable(refdata.getTradable());
+			index = Collections.binarySearch(refSymbolInfo, symbolinfo);
+			if (index < 0)
+			{
+				refSymbolInfo.add(~index, symbolinfo);
+			}
+		}
+	}
+	public List<SymbolInfo> getAllSymbolInfo(String market) 
+	{
+		ArrayList<SymbolInfo> infoList = new ArrayList<SymbolInfo>();
+		for (SymbolInfo symbolinfo : refSymbolInfo)
+		{
+			if (symbolinfo.getMarket().equals(market))
+			{
+				infoList.add(symbolinfo);
+			}
+		}
+		return infoList;
+	}
+	public int at(SymbolInfo symbolinfo) 
+	{
+		int index = Collections.binarySearch(refSymbolInfo, symbolinfo);
+		return index;
+	}
+	public SymbolInfo get(int index) {
+		if (index >= 0)
+		{
+			return refSymbolInfo.get(index);
+		}
+		else
+		{
+			return null;
+		}
+	}
+	public abstract List<SymbolInfo> getBySymbolStrings(List<String> symbolList);
+	public abstract List<SymbolInfo> getBySymbolInfos(List<SymbolInfo> inputInfoList);
 }
