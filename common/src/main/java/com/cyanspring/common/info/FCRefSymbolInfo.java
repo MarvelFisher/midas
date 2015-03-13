d2daa626f89ec7e143a0961345123951dd4930e5
steve <steve@hkfdt.com>
2015-03-13 23:08:32 +0800

    CDP fix RefSymbolInfo

--- common/src/main/java/com/cyanspring/common/info/FCRefSymbolInfo.java
+++ common/src/main/java/com/cyanspring/common/info/FCRefSymbolInfo.java
@@ -1,55 +1,15 @@
 package com.cyanspring.common.info;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.cyanspring.common.marketdata.SymbolInfo;
-import com.cyanspring.common.staticdata.RefData;
 
-public class FCRefSymbolInfo  implements IRefSymbolInfo
+public class FCRefSymbolInfo extends IRefSymbolInfo
 {
-	private List<SymbolInfo> refSymbolInfo = new ArrayList<SymbolInfo>();
-	private String serverMarket;
 	public FCRefSymbolInfo(String serverMarket)
 	{
-		this.serverMarket = serverMarket;
-	}
-
-	@Override
-	public void reset() 
-	{
-		refSymbolInfo.clear();
-	}
-	@Override
-	public void setByRefData(List<RefData> refdataList) 
-	{
-		SymbolInfo symbolinfo = null;
-		String strTmp = null;
-		int index;
-		for (RefData refdata : refdataList)
-		{
-			if (refdata.getExchange() == null || refdata.getSymbol() == null) continue;
-			
-			symbolinfo = new SymbolInfo(serverMarket, refdata.getSymbol());
-			symbolinfo.setExchange(refdata.getExchange());
-			symbolinfo.setWindCode(null);
-			symbolinfo.setHint(refdata.getRefSymbol());
-			strTmp = (refdata.getENDisplayName() == null) ? refdata.getSymbol() : refdata.getENDisplayName();
-			symbolinfo.setEnName(strTmp);
-			symbolinfo.setCnName((refdata.getCNDisplayName() == null) ? strTmp : refdata.getCNDisplayName());
-			symbolinfo.setTwName((refdata.getTWDisplayName() == null) ? strTmp : refdata.getTWDisplayName());
-			symbolinfo.setJpName(strTmp);
-			symbolinfo.setKrName(strTmp);
-			symbolinfo.setEsName(strTmp);
-			symbolinfo.setLotSize(refdata.getLotSize());
-			symbolinfo.setTickTable(refdata.getTickTable());
-			index = Collections.binarySearch(refSymbolInfo, symbolinfo);
-			if (index < 0)
-			{
-				refSymbolInfo.add(~index, symbolinfo);
-			}
-		}
+		super(serverMarket);
 	}
 	@Override
 	public List<SymbolInfo> getBySymbolStrings(List<String> symbolList) 
@@ -94,37 +54,4 @@ public class FCRefSymbolInfo  implements IRefSymbolInfo
 		}
 		return infoList;
 	}
-	@Override
-	public List<SymbolInfo> getAllSymbolInfo(String market) 
-	{
-		ArrayList<SymbolInfo> infoList = new ArrayList<SymbolInfo>();
-		for (SymbolInfo symbolinfo : refSymbolInfo)
-		{
-			if (symbolinfo.getMarket().equals(market))
-			{
-				infoList.add(symbolinfo);
-			}
-		}
-		return infoList;
-	}
-
-	@Override
-	public int at(SymbolInfo symbolinfo) 
-	{
-		int index = Collections.binarySearch(refSymbolInfo, symbolinfo);
-		return index;
-	}
-
-	@Override
-	public SymbolInfo get(int index) {
-		if (index >= 0)
-		{
-			return refSymbolInfo.get(index);
-		}
-		else
-		{
-			return null;
-		}
-	}
-
 }
