f03364b9ce7deb7bcc996a9eb769b2fb82c10694
jimmy <jimmy.cheng@hkfdt.com>
2015-03-23 12:00:48 +0800

    Add ErrorCode, and MarketSessionValidator

--- server/src/main/java/com/cyanspring/server/validation/MarketSessionValidator.java
+++ server/src/main/java/com/cyanspring/server/validation/MarketSessionValidator.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cyanspring.common.business.ParentOrder;
+import com.cyanspring.common.marketsession.MarketSessionType;
 import com.cyanspring.common.validation.IOrderValidator;
 import com.cyanspring.common.validation.OrderValidationException;
 import com.cyanspring.server.marketsession.MarketSessionManager;
@@ -17,7 +18,15 @@ public class MarketSessionValidator implements IOrderValidator{
 	
 	@Override
 	public void validate(Map<String, Object> map, ParentOrder order)
-			throws OrderValidationException {
+			throws OrderValidationException{
+		try{
+			MarketSessionType sessionType =  marketSessionManager.getCurrentSessionType();
+			if(sessionType.equals(MarketSessionType.CLOSE)){
+				throw new OrderValidationException("Market closed,order couldn't be placed");
+			 }
+		}catch(Exception e){			
+			throw new OrderValidationException("Error:"+e.getMessage());
+		}
 		
 	}
 
