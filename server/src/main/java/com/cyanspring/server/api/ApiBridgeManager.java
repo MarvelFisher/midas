983a1f2cd47a675bf9ccd0a1863e1d7472cc0532
elvis <elvis.wu@hkfdt.com>
2015-03-26 15:44:57 +0800

    Fix ApiBridgeManager bug

--- server/src/main/java/com/cyanspring/server/api/ApiBridgeManager.java
+++ server/src/main/java/com/cyanspring/server/api/ApiBridgeManager.java
@@ -252,7 +252,10 @@ public class ApiBridgeManager implements IPlugin, IAsyncEventBridge, IAsyncEvent
 			
 			socketService.setUserContext(event.getUser().getId(), record.ctx);
 
-			accountUserMap.put(event.getDefaultAccount().getId(), event.getUser().getId());
+			if(event.getDefaultAccount() != null)
+				accountUserMap.put(event.getDefaultAccount().getId(), event.getUser().getId());
+			else
+				accountUserMap.put(event.getAccounts().get(0).getId(), event.getUser().getId());
 			for(Account account: event.getAccounts()) {
 				accountUserMap.put(account.getId(), event.getUser().getId());
 			}
@@ -284,7 +287,7 @@ public class ApiBridgeManager implements IPlugin, IAsyncEventBridge, IAsyncEvent
 			quoteSubscription.put(event.getSymbol(), userSymbol);
 		}
 		
-		userSymbol.put(ctx.getId(), event.getSymbol());
+		userSymbol.put(ctx.getUser(), event.getSymbol());
 		
 		sendEventToManager(new QuoteSubEvent(ctx.getUser(), getBridgeId(), event.getSymbol()));
 	}
