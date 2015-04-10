7de5943eb0aa4a2360c7ab30ff847db6139ef322
steve <steve@hkfdt.com>
2015-04-10 16:10:15 +0800

    SendEvent tool batch file

---
+++ soak/SendEvent.bat
@@ -0,0 +1,1 @@
+java -cp jars/* com.cyanspring.soak.EventSender
---
+++ soak/SendEvent_local.bat
@@ -0,0 +1,1 @@
+java -cp target/cyanspring-soak-2.56.jar;../event/target/cyanspring-event-2.56.jar;../common/target/cyanspring-common-2.56.jar;../CSTWjars/* com.cyanspring.soak.EventSender
