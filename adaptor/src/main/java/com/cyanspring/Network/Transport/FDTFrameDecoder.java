8645e806bf2af88cc9320d5f6ac8ec48aa1a9f2e
calvin <calvin@hkfdt.com>
2015-04-23 14:55:35 +0800

    add MsgPackLite

---
+++ adaptor/src/main/java/com/cyanspring/Network/Transport/FDTFrameDecoder.java
@@ -0,0 +1,52 @@
+package com.cyanspring.Network.Transport;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+public class FDTFrameDecoder extends ByteToMessageDecoder {
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf in,List<Object> out) throws Exception {
+		while(in.readableBytes() > (FDTPacket.PKT_HEAD_SIZE + FDTPacket.PKT_TAIL_SIZE)) {
+			//packet_head(4) + packet_tail(2)
+		
+		
+			in.markReaderIndex();
+			byte head_byte = in.readByte();
+			if(head_byte != FDTPacket.PKT_LEAD) {			
+				continue; //discard head byte that is not 0x02;
+			}
+			
+			byte ptyp_byte = in.readByte();
+			int packetlen_low = unsignedByteToInt(in.readByte());
+			int packetlen_hi = unsignedByteToInt(in.readByte());
+			int packetlen = packetlen_low + packetlen_hi*256;
+			if(in.readableBytes() < (packetlen + FDTPacket.PKT_HEAD_SIZE + FDTPacket.PKT_TAIL_SIZE)) {			
+				in.resetReaderIndex(); //more data needed;
+				break;
+			}
+			
+			byte[] body_buf = new byte[packetlen];
+			in.readBytes(body_buf);
+			
+			
+			byte crc_byte = in.readByte();  //skill checking crc
+			byte tail_byte = in.readByte();
+					
+			if(tail_byte != FDTPacket.PKT_END) {
+				//discard tail byte that is not 0x03, skill one byte, keep checking head byte(0x02)
+				in.resetReaderIndex();
+				in.readByte();
+				continue; 
+			}
+			out.add(body_buf);
+		}
+	}
+	
+	public static int unsignedByteToInt(byte b) {  
+	    return (int) b & 0xFF;  
+	}  
+}
---
+++ adaptor/src/main/java/com/cyanspring/adaptor/future/wind/gateway/MsgPackLiteServer.java
@@ -0,0 +1,143 @@
+package com.cyanspring.adaptor.future.wind.gateway;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cyanspring.Network.Transport.FDTFrameDecoder;
+import com.cyanspring.Network.Transport.FDTFrameEncoder;
+
+
+public class MsgPackLiteServer implements Runnable {
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(com.cyanspring.adaptor.future.wind.gateway.MsgPackLiteServer.class);
+	
+	private int serverPort = 10048;
+	public static  int readIdleInterval = 7;
+	public static  int writeIdleInterval = 10;
+	
+	private Channel channel = null;
+	private boolean bAlive = true;
+	
+
+	@Override
+	public void run() {
+		runNetty();
+	}	
+	
+	public void stop() {
+		if(this.channel != null) {
+			this.channel.close();
+		}
+		bAlive = false;
+	}
+	
+	private void runNetty() {
+		int threads = 16;
+		EventLoopGroup bossGroup = new NioEventLoopGroup(2);
+		EventLoopGroup workerGroup = new NioEventLoopGroup(threads);
+
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class) // (3)
+             .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                 @Override
+                 public void initChannel(SocketChannel ch) throws Exception {
+                	 	ch.pipeline().addLast("decoder",new FDTFrameDecoder());
+                	 	ch.pipeline().addLast("encoder",new FDTFrameEncoder());
+                		ch.pipeline().addLast("idleStateHandler", new IdleStateHandler(readIdleInterval, writeIdleInterval, 0));
+                		ch.pipeline().addLast("idleHandler", new idDataServerIdleHandler());                	
+                		//ch.pipeline().addLast(new idDataServerHandler());
+                 }
+             })
+             .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+             .childOption(ChannelOption.SO_KEEPALIVE, true)  // (6)
+             .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,3000)
+             .option(ChannelOption.SO_REUSEADDR,false)
+             .childOption(ChannelOption.TCP_NODELAY, true);
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f;
+
+            while(bAlive) {
+            	try {
+            		f = b.bind(serverPort).sync();
+            		if(f.isSuccess()) {
+            			log.info("Wind Message Pack Lite Data Server listen at port : " + serverPort + " , max worker threads : " + threads);
+
+            			// Wait until the server socket is closed.
+            			// In this example, this does not happen, but you can do that to gracefully
+            			// shut down your server.
+            			f.channel().closeFuture().sync();
+            		
+            		}
+            	} catch(Exception e) {
+            		log.warn(e.getMessage(),e);
+            	}
+            	log.warn("Wind Message Pack Lite Data server can not bind at port : " + serverPort + " , will try again after 3 seconds.");
+            	if(bAlive) {
+            		Thread.sleep(3000);
+            	}
+            }
+			
+		} catch (Exception e) {
+			log.warn(e.getMessage(),e);
+		}
+        finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }		
+	}
+	
+	public int getServerPort() {
+		return serverPort;
+	}
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}	
+
+	public int getReadIdleInterval() {
+		return readIdleInterval;
+	}
+	public void setReadIdleInterval(int interval) {
+		readIdleInterval = interval;
+	}
+	
+	public int getWriteIdleInterval() {
+		return writeIdleInterval;
+	}
+	public void setWriteIdleInterval(int interval) {
+		writeIdleInterval = interval;
+	}	
+}
+
+class idDataServerIdleHandler extends ChannelDuplexHandler {
+	private static final Logger log = LoggerFactory
+			.getLogger(idDataServerIdleHandler.class);
+	@Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent e = (IdleStateEvent) evt;
+            if (e.state() == IdleState.READER_IDLE) {            	
+            		log.info("Read Idle for " + MsgPackLiteServer.readIdleInterval + " seconds , close client : " + ctx.channel().remoteAddress());
+            		ctx.close();            	
+            } 
+        }
+    } 	
+}
