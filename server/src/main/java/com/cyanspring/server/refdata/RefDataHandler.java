deb24441f8de3f16fb2c6c3c216dd1d79b864d64
elvis <elvis.wu@hkfdt.com>
2015-04-29 13:20:51 +0800

    Fix refDataHandler init bug

--- server/src/main/java/com/cyanspring/server/refdata/RefDataHandler.java
+++ server/src/main/java/com/cyanspring/server/refdata/RefDataHandler.java
@@ -10,13 +10,10 @@ import com.cyanspring.common.event.refdata.RefDataRequestEvent;
 import com.cyanspring.common.marketsession.MarketSessionType;
 import com.cyanspring.common.staticdata.FuRefDataManager;
 import com.cyanspring.common.staticdata.IRefDataManager;
-import com.cyanspring.common.util.TimeUtil;
 import com.cyanspring.event.AsyncEventProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Date;
 
 /**
  * Description....
@@ -63,7 +60,7 @@ public class RefDataHandler implements IPlugin {
 
     public void processRefDataRequestEvent(RefDataRequestEvent event) {
         try {
-            eventManager.sendLocalOrRemoteEvent(new RefDataEvent(null, null, refDataManager.getRefDataList()));
+            eventManager.sendLocalOrRemoteEvent(new RefDataEvent(event.getKey(), event.getSender(), refDataManager.getRefDataList()));
         } catch (Exception e) {
             log.warn(e.getMessage(), e);
         }
@@ -75,7 +72,14 @@ public class RefDataHandler implements IPlugin {
         if (currentType.equals(event.getSession()) || !MarketSessionType.PREOPEN.equals(event.getSession()))
             return;
         currentType = event.getSession();
-        initRefData(true);
+        if (refDataManager instanceof FuRefDataManager) {
+            try {
+                refDataManager.init();
+                eventManager.sendGlobalEvent(new RefDataEvent(null, null, refDataManager.getRefDataList()));
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+        }
     }
 
     @Override
@@ -88,23 +92,11 @@ public class RefDataHandler implements IPlugin {
         if (eventProcessor.getThread() != null)
             eventProcessor.getThread().setName("RefDataHandler");
 
-        initRefData(false);
+        refDataManager.init();
     }
 
     @Override
     public void uninit() {
         eventProcessor.uninit();
-    }
-
-    private void initRefData(boolean boardcast){
-        if (refDataManager instanceof FuRefDataManager) {
-            try {
-                refDataManager.init();
-                if (boardcast)
-                    eventManager.sendGlobalEvent(new RefDataEvent(null, null, refDataManager.getRefDataList()));
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
-            }
-        }
     }
 }
