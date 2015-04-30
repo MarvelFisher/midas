6f99af88ba9d5adf7b1b9ab1b899076b6e0775f4
claudia <claudia.zhou@hkfdt.com>
2015-04-30 14:31:41 +0800

    Fix method name.

--- server/src/main/java/com/cyanspring/server/account/AccountPositionManager.java
+++ server/src/main/java/com/cyanspring/server/account/AccountPositionManager.java
@@ -628,7 +628,7 @@ public class AccountPositionManager implements IPlugin {
         }
     }
 
-    public void processQuotExtEvent(QuoteExtEvent event) {
+    public void processQuoteExtEvent(QuoteExtEvent event) {
 
         marketExtData.put(event.getSymbol(), event);
     }
