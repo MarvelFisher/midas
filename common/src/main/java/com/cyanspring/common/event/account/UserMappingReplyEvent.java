dce09c4c78ad8e291ce80f8c2424ebc731224c5a
claudia <claudia.zhou@hkfdt.com>
2015-05-05 11:25:03 +0800

    Add market and language fields to UserMapping.

--- common/src/main/java/com/cyanspring/common/event/account/UserMappingReplyEvent.java
+++ common/src/main/java/com/cyanspring/common/event/account/UserMappingReplyEvent.java
@@ -9,15 +9,19 @@ public class UserMappingReplyEvent extends RemoteAsyncEvent {
     private String userThirdParty;
     private boolean userExist;
     private boolean userThirdPartyExist;
+    private String market;
+    private String language;
     private String clientId;
 
-    public UserMappingReplyEvent(String key, String receiver, String txId, String user, String userThirdParty, boolean userExist, boolean userThirdPartyExist, String clientId) {
+    public UserMappingReplyEvent(String key, String receiver, String txId, String user, String userThirdParty, boolean userExist, boolean userThirdPartyExist, String market, String language, String clientId) {
         super(key, receiver);
         this.txId = txId;
         this.user = user;
         this.userThirdParty = userThirdParty;
         this.userExist = userExist;
         this.userThirdPartyExist = userThirdPartyExist;
+        this.market = market;
+        this.language = language;
         this.clientId = clientId;
     }
 
@@ -43,5 +47,13 @@ public class UserMappingReplyEvent extends RemoteAsyncEvent {
 
     public String getClientId() {
         return clientId;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public String getLanguage() {
+        return language;
     }
 }
