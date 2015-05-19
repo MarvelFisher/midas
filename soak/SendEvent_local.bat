11140f6d339bdf4dc5e19af0ac69049b530e19b0
steve <steve@hkfdt.com>
2015-05-19 18:28:53 +0800

    modify EventSender run with 2 args

--- soak/SendEvent_local.bat
+++ soak/SendEvent_local.bat
@@ -1,1 +1,1 @@
-java -cp target/cyanspring-soak-2.56.jar;../event/target/cyanspring-event-2.56.jar;../common/target/cyanspring-common-2.56.jar;../CSTWjars/* com.cyanspring.soak.EventSender
+java -cp target/cyanspring-soak-2.56.jar;../event/target/cyanspring-event-2.56.jar;../common/target/cyanspring-common-2.56.jar;../CSTWjars/* com.cyanspring.soak.EventSender conf/evts_log4j.xml conf/eventSender.xml
