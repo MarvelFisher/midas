e2c2220339078c65ecbfa1a9bf0649c95a5d1ced
elvis <elvis.wu@hkfdt.com>
2015-05-20 14:18:31 +0800

    Update SendEvent shell script

--- soak/SendEvent.sh
+++ soak/SendEvent.sh
@@ -8,33 +8,40 @@ fi
 java -cp "jars/*" com.cyanspring.soak.EventSender $1 $2
 
 echo "Start canceling pending orders..."
-sleep 30
+sleep 20
 
-HOME = ~ubuntu
-if [ ! -d "$HOME/appServer" ]; then
+cd ~ubuntu
+
+if [ ! -d "appServer/" ]; then
     echo "Directory is not exist, cannot shutdown appServer."
 else
-    cd $HOME
     cd appServer/
+    echo "Starting shutdown appServer"
     ./run.sh stop
 fi
 
-if [ ! -d "$HOME/LTS" ]; then
+sleep 3
+
+cd ..
+
+if [ ! -d "LTS/" ]; then
     echo "Directory is not exist, cannot shutdown LTSServer."
 else
-    cd $HOME
     cd LTS/
 
     if [ -s LTS-INFO_PID ]; then
         INFO_PID=`cat LTS-INFO_PID`
-        kill $INFO_PID
+        echo "Starting shutdown LTS-INFO server, PID: $INFO_PID"
+        kill -9 $INFO_PID
     else
         echo "Cannot find LTS-INFO PID to shutdown server."
     fi
 
+    sleep 3
+
     if [ -s LTS_PID ]; then
         LTS_PID=`cat LTS_PID`
-        echo "Starting shutdown LTSServer."
+        echo "Starting shutdown LTSServer, PID: $LTS_PID"
         kill $LTS_PID
     else
         echo "Cannot find LTS PID to shutdown server."
