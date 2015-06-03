2546f5e968be339510b965888e09f79ee5e4d7c6
claudia <claudia.zhou@hkfdt.com>
2015-06-03 13:45:04 +0800

    Add integer value to AccountState enum.

--- common/src/main/java/com/cyanspring/common/account/AccountState.java
+++ common/src/main/java/com/cyanspring/common/account/AccountState.java
@@ -1,7 +1,30 @@
 package com.cyanspring.common.account;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum AccountState {
-	ACTIVE,
-	FROZEN,
-	TERMINATED;
+	ACTIVE(0),
+	FROZEN(1),
+	TERMINATED(2);
+
+	private final int value;
+	AccountState(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	static Map<Integer, AccountState> map = new HashMap<>();
+	static {
+		for (AccountState status : values()) {
+			map.put(status.getValue(), status);
+		}
+	}
+
+	public static AccountState fromInt(int value){
+		return map.get(value);
+	}
 }
