dc909b34533649a4b5cbbdce2c1fbf1049efcec9
dennis <dennis.chen@hkfdt.com>
2015-06-08 11:28:04 +0800

    added CTP adaptor framework

---
+++ adaptor/src/main/java/com/cyanspring/adaptor/future/ctp/CtpAdaptor.java
@@ -0,0 +1,68 @@
+package com.cyanspring.adaptor.future.ctp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cyanspring.common.downstream.IDownStreamConnection;
+import com.cyanspring.common.stream.IStreamAdaptor;
+
+public class CtpAdaptor implements IStreamAdaptor<IDownStreamConnection> {
+	private static final Logger log = LoggerFactory
+			.getLogger(CtpAdaptor.class);
+
+	private String url;
+	private String conLog;
+	private String connectionPrefix = "CTP";
+	private int connectionCount = 2;
+	private List<IDownStreamConnection> connections = new ArrayList<IDownStreamConnection>();
+
+	@Override
+	public void init() throws Exception {
+		for(int i=0; i < connectionCount; i++) {
+			connections.add(new CtpConnection(connectionPrefix + i, url, conLog));
+		}
+
+		for(IDownStreamConnection connection: connections)
+			connection.init();
+	}
+
+	@Override
+	public void uninit() {
+		for(IDownStreamConnection connection: connections)
+			connection.uninit();
+	}
+
+	@Override
+	public List<IDownStreamConnection> getConnections() {
+		return connections;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getConLog() {
+		return conLog;
+	}
+
+	public void setConLog(String conLog) {
+		this.conLog = conLog;
+	}
+
+	public int getConnectionCount() {
+		return connectionCount;
+	}
+
+	public void setConnectionCount(int connectionCount) {
+		this.connectionCount = connectionCount;
+	}
+
+	
+}
---
+++ adaptor/src/main/java/com/cyanspring/adaptor/future/ctp/CtpConnection.java
@@ -0,0 +1,92 @@
+package com.cyanspring.adaptor.future.ctp;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cyanspring.common.business.ChildOrder;
+import com.cyanspring.common.downstream.DownStreamException;
+import com.cyanspring.common.downstream.IDownStreamConnection;
+import com.cyanspring.common.downstream.IDownStreamListener;
+import com.cyanspring.common.downstream.IDownStreamSender;
+
+public class CtpConnection implements IDownStreamConnection {
+	private static final Logger log = LoggerFactory
+			.getLogger(CtpConnection.class);
+
+	private String url;
+	private String conLog;
+	private String id;
+	private boolean state; 
+	private IDownStreamListener listener;
+	
+	public CtpConnection(String id, String url, String conLog) {
+		this.id = id;
+		this.url = url;
+		this.conLog = conLog;
+	}
+	
+	@Override
+	public void init() throws Exception {
+		// TODO does login and initialization
+
+	}
+
+	@Override
+	public void uninit() {
+		// TODO clean up
+
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public boolean getState() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	class DownStreamSender implements IDownStreamSender {
+		@Override
+		public void newOrder(ChildOrder order) throws DownStreamException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void amendOrder(ChildOrder order, Map<String, Object> fields)
+				throws DownStreamException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void cancelOrder(ChildOrder order) throws DownStreamException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean getState() {
+			return CtpConnection.this.getState();
+		}
+		
+	}
+	
+	@Override
+	public IDownStreamSender setListener(IDownStreamListener listener)
+			throws DownStreamException {
+		
+		if(listener != null && this.listener != null)
+			throw new DownStreamException("Support only one listener");
+		
+		this.listener = listener;
+		return new DownStreamSender();
+	}
+
+
+}
