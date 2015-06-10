97b75c4692727976509001669d3cc562500cd0ff
claudia <claudia.zhou@hkfdt.com>
2015-06-10 16:26:59 +0800

    Add UserLoginType field to UserLoginEvent.

---
+++ common/src/main/java/com/cyanspring/common/account/UserLoginType.java
@@ -0,0 +1,31 @@
+package com.cyanspring.common.account;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum UserLoginType {
+
+    USER_ID(0),
+    EMAIL(1),
+    PHONE(2);
+
+    private final int value;
+    UserLoginType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    static Map<Integer, UserLoginType> map = new HashMap<>();
+    static {
+        for (UserLoginType status : values()) {
+            map.put(status.getValue(), status);
+        }
+    }
+
+    public static UserLoginType fromInt(int value){
+        return map.get(value);
+    }
+}
