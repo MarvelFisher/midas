ea2dc4bbe1a9d2d4fa017d0aeabcd0b0e5df75d3
jimmy <jimmy>
2015-06-15 18:12:21 +0800

    remove space line

--- server/src/main/java/com/cyanspring/server/statistic/AccountStatisticBean.java
+++ server/src/main/java/com/cyanspring/server/statistic/AccountStatisticBean.java
@@ -12,93 +12,53 @@ public class AccountStatisticBean {
 	public double totalDailyPnL;
 	public double totalAccountValue;
 	public double totalCashDeposited;
-	
-	
-	
-	
+
 	public double getTotalUrPnL() {
 		return totalUrPnL;
 	}
-
-
-
 
 	public void setTotalUrPnL(double totalUrPnL) {
 		this.totalUrPnL = totalUrPnL;
 	}
 
-
-
-
 	public double getTotalAllTimePnL() {
 		return totalAllTimePnL;
 	}
-
-
-
 
 	public void setTotalAllTimePnL(double totalAllTimePnL) {
 		this.totalAllTimePnL = totalAllTimePnL;
 	}
 
-
-
-
 	public double getTotalValue() {
 		return totalValue;
 	}
-
-
-
 
 	public void setTotalValue(double totalValue) {
 		this.totalValue = totalValue;
 	}
 
-
-
-
 	public double getTotalDailyPnL() {
 		return totalDailyPnL;
 	}
-
-
-
 
 	public void setTotalDailyPnL(double totalDailyPnL) {
 		this.totalDailyPnL = totalDailyPnL;
 	}
 
-
-
-
 	public double getTotalAccountValue() {
 		return totalAccountValue;
 	}
-
-
-
 
 	public void setTotalAccountValue(double totalAccountValue) {
 		this.totalAccountValue = totalAccountValue;
 	}
 
-
-
-
 	public double getTotalCashDeposited() {
 		return totalCashDeposited;
 	}
 
-
-
-
 	public void setTotalCashDeposited(double totalCashDeposited) {
 		this.totalCashDeposited = totalCashDeposited;
 	}
-
-
-
-
 
 }
