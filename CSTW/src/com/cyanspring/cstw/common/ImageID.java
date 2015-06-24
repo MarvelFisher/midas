8e64ba1fb78be3ac7b0484fa38d2ef17ceede292
jimmy <jimmy>
2015-06-24 11:58:45 +0800

    CSTW : add icon refresh

--- CSTW/src/com/cyanspring/cstw/common/ImageID.java
+++ CSTW/src/com/cyanspring/cstw/common/ImageID.java
@@ -24,7 +24,7 @@ public enum ImageID {
 	PIN_ICON("icons/pin.png"),
 	CANCEL_ICON("icons/cancel.png"),
 	SAVE_ICON("icons/save.png"),
-	REFRESH_ICON("icons/amend2.png"),
+	REFRESH_ICON("icons/refresh.png"),
 	LINE_ICON("icons/lineIcon.png")
 	;
 	private String value;
