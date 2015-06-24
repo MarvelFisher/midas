2cbc659a9f16605bf70bc7c6dbb0fb71bc722f5f
jimmy <jimmy>
2015-06-24 14:03:58 +0800

    CSTW : add Open Position Statistic View

--- CSTW/src/com/cyanspring/cstw/gui/PositionStatisticView.java
+++ CSTW/src/com/cyanspring/cstw/gui/PositionStatisticView.java
@@ -6,8 +6,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.SWT;
@@ -18,10 +20,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.cyanspring.common.BeanHolder;
+import com.cyanspring.common.account.ClosedPosition;
 import com.cyanspring.common.account.OpenPosition;
 import com.cyanspring.common.event.AsyncEvent;
+import com.cyanspring.common.event.AsyncTimerEvent;
 import com.cyanspring.common.event.IAsyncEventListener;
-import com.cyanspring.common.event.IAsyncExecuteEventListener;
 import com.cyanspring.common.event.RemoteAsyncEvent;
 import com.cyanspring.common.event.account.AllPositionSnapshotReplyEvent;
 import com.cyanspring.common.event.account.AllPositionSnapshotRequestEvent;
@@ -30,7 +33,8 @@ import com.cyanspring.common.event.account.ExecutionUpdateEvent;
 import com.cyanspring.common.event.account.OpenPositionDynamicUpdateEvent;
 import com.cyanspring.common.event.account.OpenPositionUpdateEvent;
 import com.cyanspring.common.event.order.ClosePositionReplyEvent;
-import com.cyanspring.common.event.order.ClosePositionRequestEvent;
+import com.cyanspring.common.event.statistic.AccountNumberReplyEvent;
+import com.cyanspring.common.event.statistic.AccountNumberRequestEvent;
 import com.cyanspring.cstw.business.Business;
 import com.cyanspring.cstw.common.ImageID;
 import com.cyanspring.cstw.gui.bean.PositionStatisticBean;
@@ -42,59 +46,115 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 
 	private static final Logger log = LoggerFactory.getLogger(PositionStatisticView.class);
 	public static final String ID = "com.cyanspring.cstw.gui.PositionStatisticView";
-//	private Composite topComposite;
 	private DynamicTableViewer openPositionViewer;			
-
+	private int limitAccount = 2000;
 	private Action aggregateAction;
 	private Action refreshAction;
 	private ImageRegistry imageRegistry;
 	private final String COLUMN_CLASS="CLASS";
+	private ReentrantLock lock = new ReentrantLock();
 	private Map <String,PositionStatisticBean> symbolOpMap = new HashMap<String, PositionStatisticBean>();
-	private ConcurrentHashMap <String,List<OpenPosition>> accountOpMap = new ConcurrentHashMap<String,List<OpenPosition>>();
+	private ConcurrentHashMap <String,List<OpenPosition>> accountOpMap = new ConcurrentHashMap<String,List<OpenPosition>>();	
+	private AsyncTimerEvent refreshEvent = new AsyncTimerEvent();
+	private long maxRefreshInterval = 1000;
+	
 	@Override
 	public void onEvent(AsyncEvent event) {
 		
-		if( event instanceof AllPositionSnapshotReplyEvent){
-			log.info("get reply AllPositionSnapshotReplyEvent");
+		if(event instanceof AsyncTimerEvent){
+			
+			displayOpenPosition();		
+			
+		}else if( event instanceof AllPositionSnapshotReplyEvent){
+			
 			AllPositionSnapshotReplyEvent allPosition = (AllPositionSnapshotReplyEvent) event;
 			List <OpenPosition>ops = allPosition.getOpenPositionList();
 			collectAllPosition(ops);
 			displayOpenPosition();
+			
 		}else if(event instanceof OpenPositionUpdateEvent){
-			log.info("get reply OpenPositionUpdateEvent");
+			
 			OpenPosition position = ((OpenPositionUpdateEvent) event).getPosition();
-			caculate(position);
+			updatePosition(position);
 			
 		}else if(event instanceof OpenPositionDynamicUpdateEvent){
-			log.info("get reply OpenPositionDynamicUpdateEvent");
+			
 			OpenPosition position = ((OpenPositionDynamicUpdateEvent) event).getPosition();
-			caculate(position);
+			updatePosition(position);
+			
 		}else if(event instanceof ClosedPositionUpdateEvent){
-			log.info("get reply Clo"
-					+ "sedPositionUpdateEvent");
-
+			
+			ClosedPosition position = ((ClosedPositionUpdateEvent) event).getPosition();
+			closePosition(position);	
 		
-		}else if(event instanceof ExecutionUpdateEvent){
-			log.info("get reply ExecutionUpdateEvent");
-
-		
-		}else if(event instanceof OpenPositionDynamicUpdateEvent){
-			log.info("get reply OpenPositionDynamicUpdateEvent");
-
-
 		}else if(event instanceof ClosePositionReplyEvent){
-			log.info("get reply ClosePositionReplyEvent");
+			
+			ClosedPosition position = ((ClosedPositionUpdateEvent) event).getPosition();
+			closePosition(position);
 
+		}else if(event instanceof AccountNumberReplyEvent){
+			
+			AccountNumberReplyEvent accountNum = (AccountNumberReplyEvent) event;
+			if(accountNum.isOk()){
+				log.info("Account Number:{}",accountNum.getAccountNumber());
+				if( limitAccount > accountNum.getAccountNumber()){
+					sendAllPositionRequestEvent();
+				}else{
+					log.error("Total account :"+accountNum.getAccountNumber()+" exceed account limit:"+limitAccount);
+					showDialog("Total account :"+accountNum.getAccountNumber()+" exceed account limit:"+limitAccount);
+					refreshAction.setEnabled(false);
+				}
+			}else{
+				log.error(accountNum.getErrorMessage());
+				showDialog(accountNum.getErrorMessage());
+				refreshAction.setEnabled(false);
+			}
+		}
+	}
+
+	private void closePosition(ClosedPosition position) {
 		
+		String account = position.getAccount();
+		List<OpenPosition> tempList = new ArrayList<OpenPosition>();
+		if(accountOpMap.containsKey(account)){
+			List <OpenPosition>oldList = accountOpMap.get(account);
+			for(OpenPosition op:oldList){
+				if(!position.getSymbol().equals(op.getSymbol())){
+					tempList.add(op);
+				}
+			}
+			accountOpMap.put(account, tempList);
+		}		
+	}
+
+	private void updatePosition(OpenPosition position) {
+		
+		String account = position.getAccount();
+		List<OpenPosition> tempList = new ArrayList<OpenPosition>();
+		if(accountOpMap.containsKey(account)){
+			List <OpenPosition>oldList = accountOpMap.get(account);
+			for(OpenPosition op:oldList){
+				if(position.getSymbol().equals(op.getSymbol())){
+					tempList.add(position);
+				}else{
+					tempList.add(op);
+				}
+			}
+			accountOpMap.put(account, tempList);
+		}else{
+			tempList = new ArrayList<OpenPosition>();
+			tempList.add(position);
+			accountOpMap.put(account, tempList);
 		}
 	}
 
 	private void collectAllPosition(List<OpenPosition> ops) {
 		
+		log.info("Collect All position:{}",ops.size());
 		for(OpenPosition op:ops){			
 			List <OpenPosition>opList = null;
 			String account = op.getAccount();
-			log.info("account:{},position:{}",account,op.getSymbol());
+			//log.info("account:{},position:{}",account,op.getSymbol());
 			if(accountOpMap.containsKey(account)){
 				opList = accountOpMap.get(account);
 				opList.add(op);
@@ -109,7 +169,7 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 	private void caculate(OpenPosition op) {
 		PositionStatisticBean bean = null;
 		String symbol = op.getSymbol();
-		log.info("Account:{}, symbol:{} ,Qty:{},Avaliable Qty:{}",new Object[]{op.getAccount(),symbol,op.getQty(),op.getAvailableQty()});
+//		log.info("Account:{}, symbol:{} ,Qty:{},Avaliable Qty:{}",new Object[]{op.getAccount(),symbol,op.getQty(),op.getAvailableQty()});
 		if(symbolOpMap.containsKey(symbol)){
 			bean = symbolOpMap.get(op.getSymbol());
 			bean.setQty(op.getQty()+bean.getQty());
@@ -124,69 +184,68 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 		if(null != bean)
 			symbolOpMap.put(symbol, bean);
 	}
-
+	
 	@Override
 	public void createPartControl(Composite parent) {
-		
+	
 		subEvent(AllPositionSnapshotReplyEvent.class);
-		subEvent(OpenPositionUpdateEvent.class);
-		subEvent(OpenPositionDynamicUpdateEvent.class);
-		
-		subEvent(ClosedPositionUpdateEvent.class);
-		subEvent(ExecutionUpdateEvent.class);
-		subEvent(OpenPositionDynamicUpdateEvent.class);
-//		subEvent(ClosePositionReplyEvent.class,Business.getInstance().getFirstServer());
-		
-//		Business.getInstance().getEventManager().subscribe(ClosePositionReplyEvent.class, 
-//				"test9-FX",(IAsyncExecuteEventListener)PositionView.class );
+		subEvent(AccountNumberReplyEvent.class);
+		subEvent(OpenPositionUpdateEvent.class,null);
+		subEvent(OpenPositionDynamicUpdateEvent.class,null);		
+		subEvent(ClosedPositionUpdateEvent.class,null);
+		subEvent(ExecutionUpdateEvent.class,null);
+		subEvent(ClosePositionReplyEvent.class,null);
 		
 		imageRegistry = Activator.getDefault().getImageRegistry();
-
-
-		
-		final Composite mainComposite = new Composite(parent,SWT.BORDER);
+		final Composite  mainComposite = new Composite(parent,SWT.BORDER);
 		mainComposite.setLayout (new FillLayout());
-
-		
+	
 		createOpenPositionViewer(mainComposite);
 		createAggregateAction(parent);
 		createRefreshAction(parent);
 		
-		log.info("send AllPositionSnapshotRequestEvent");
-		sendAllPositionRequestEvent();
-	
+		AccountNumberRequestEvent accountNumberEvent = new AccountNumberRequestEvent(ID, Business.getInstance().getFirstServer());
+		sendRemoteEvent(accountNumberEvent);
 	}
 	private void sendAllPositionRequestEvent() {
 		
 		accountOpMap.clear();
-		symbolOpMap.clear();
-		
-		AllPositionSnapshotRequestEvent request = new AllPositionSnapshotRequestEvent(
-		ID, Business.getInstance().getFirstServer());
+		symbolOpMap.clear();	
+		AllPositionSnapshotRequestEvent request = new AllPositionSnapshotRequestEvent(ID, Business.getInstance().getFirstServer());
 		sendRemoteEvent(request);		
 	}
 
 	public void displayOpenPosition(){
-		if(!aggregateAction.isChecked()){
-			log.info("showAllOpenPosition");
-			showAllOpenPosition();
-		}else{
-			log.info("showAggregateOpenPosition");
-			showAggregateOpenPosition();
+		
+		try{
+			lock.lock();
+			if(!aggregateAction.isChecked()){
+				showAllOpenPosition();
+			}else{
+				showAggregateOpenPosition();
+			}
+		}finally{
+			lock.unlock();
 		}
 	}
 	
 	
 	
 	private void showAggregateOpenPosition() {
+		
 		symbolOpMap = new HashMap<String, PositionStatisticBean>();
 		final List <OpenPosition>opList = getAllOpenPositionList();
+		
 		for(OpenPosition op : opList){
 			caculate(op);
-		}
+		}		
 		
 		final List <PositionStatisticBean>psbList = new ArrayList<PositionStatisticBean>();
 		psbList.addAll(symbolOpMap.values());	
+		
+		if( null == psbList || psbList.isEmpty()){
+			return;
+		}
 		
 		openPositionViewer.getControl().getDisplay().asyncExec(new Runnable() {
 			@Override
@@ -197,14 +256,10 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 						return;
 					
 					List<ColumnProperty> properties = openPositionViewer
-							.setObjectColumnProperties(psbList.get(0));
-					
+							.setObjectColumnProperties(psbList.get(0));				
 					properties = filterColumn(properties);
-
-					openPositionViewer.setSmartColumnProperties(psbList.get(0).getClass().getName(),
-							properties);
-					openPositionViewer.setInput(psbList);
-				
+					openPositionViewer.setSmartColumnProperties(psbList.get(0).getClass().getName(),properties);
+					openPositionViewer.setInput(psbList);			
 					openPositionViewer.refresh();
 				}
 			}
@@ -213,10 +268,11 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 	}
 
 	private void showAllOpenPosition() {
-		log.info("accountOpMap:{}",accountOpMap.size());
+		
 		final List <OpenPosition>psbList = getAllOpenPositionList();
-
-		log.info("psbList:{}",psbList.size());
+		if( null == psbList || psbList.isEmpty()){
+			return;
+		}
 		openPositionViewer.getControl().getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
@@ -227,28 +283,15 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 					
 					List<ColumnProperty> properties = openPositionViewer
 							.setObjectColumnProperties(psbList.get(0));
-					openPositionViewer.setSmartColumnProperties(psbList.get(0).getClass().getName(),
-							properties);
+					openPositionViewer.setSmartColumnProperties(psbList.get(0).getClass().getName(),properties);
 					openPositionViewer.setInput(psbList);
-					
-//					
-//					if(null == PositionStatisticView.this.symbolOpMap || openPositionViewer.isViewClosing())
-//						return;
-//					
-//					if(PositionStatisticView.this.symbolOpMap.size() > 0)
-//						createOpenPositionColumns(psbList);
-//					
-//					openPositionViewer.setInput(psbList);
-
 					openPositionViewer.refresh();
 				}
 			}
 		});
-		
-		
-		
 	}
 	private List<ColumnProperty> filterColumn(List<ColumnProperty> properties){
+		
 		List<ColumnProperty>  propList= new ArrayList<ColumnProperty>();
 		for(ColumnProperty cp : properties){
 			if(!cp.getTitle().toUpperCase().equals(COLUMN_CLASS)){
@@ -259,6 +302,7 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 	}
 	
 	private List<OpenPosition> getAllOpenPositionList(){
+		
 		final List <OpenPosition>psbList = new ArrayList<OpenPosition>();
 		Iterator<List<OpenPosition>> opIterator = accountOpMap.values().iterator();
 		while(opIterator.hasNext()){			
@@ -268,18 +312,21 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 	}
 	private void createRefreshAction(final Composite parent){
 		
-		refreshAction = new StyledAction("", org.eclipse.jface.action.IAction.AS_PUSH_BUTTON) {
+		refreshAction = new StyledAction("", org.eclipse.jface.action.IAction.AS_CHECK_BOX) {
 			public void run() {
-				sendAllPositionRequestEvent();
+				if(refreshAction.isChecked()){
+					scheduleJob(refreshEvent, maxRefreshInterval);
+				}else{
+					cancelScheduleJob(refreshEvent);
+				}
 			}
 		};
 		
-		refreshAction.setChecked(false);		
-		refreshAction.setText("Refreash");
-		refreshAction.setToolTipText("Refreash");	
+		refreshAction.setChecked(true);		
+		refreshAction.setText("Auto Refresh");
+		refreshAction.setToolTipText("Auto Refresh");	
 		ImageDescriptor imageDesc = imageRegistry.getDescriptor(ImageID.REFRESH_ICON.toString());	
-		refreshAction.setImageDescriptor(imageDesc);
-		
+		refreshAction.setImageDescriptor(imageDesc);	
 		getViewSite().getActionBars().getToolBarManager().add(refreshAction);
 	}
 	private void createAggregateAction(final Composite parent){
@@ -292,22 +339,23 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 					showAggregateOpenPosition();
 				}
 			}
-		};
+		};	
 		
 		aggregateAction.setChecked(false);		
 		aggregateAction.setText("Aggregate");
 		aggregateAction.setToolTipText("Aggregate");	
 		ImageDescriptor imageDesc = imageRegistry.getDescriptor(ImageID.FILTER_ICON.toString());	
-		aggregateAction.setImageDescriptor(imageDesc);
-		
+		aggregateAction.setImageDescriptor(imageDesc);		
 		getViewSite().getActionBars().getToolBarManager().add(aggregateAction);
 	}
+	
 	private void createOpenPositionViewer(Composite parent) {
 	    String strFile = Business.getInstance().getConfigPath() + "OpenPositionTable.xml";
 		openPositionViewer = new DynamicTableViewer(parent, SWT.MULTI | SWT.FULL_SELECTION | SWT.H_SCROLL
 				| SWT.V_SCROLL, Business.getInstance().getXstream(), strFile, BeanHolder.getInstance().getDataConverter());
 		openPositionViewer.init();
 	}
+	
 	private void sendRemoteEvent(RemoteAsyncEvent event) {
 		try {
 			Business.getInstance().getEventManager().sendRemoteEvent(event);
@@ -315,6 +363,7 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 			log.error(e.getMessage(), e);
 		}
 	}
+	
 	@Override
 	public void setFocus() {
 		
@@ -323,46 +372,28 @@ public class PositionStatisticView extends ViewPart implements IAsyncEventListen
 	private void subEvent(Class<? extends AsyncEvent> clazz){
 		Business.getInstance().getEventManager().subscribe(clazz,ID, this);		
 	}
+	
 	private void subEvent(Class<? extends AsyncEvent> clazz,String ID){
 		Business.getInstance().getEventManager().subscribe(clazz,ID, this);		
 	}
-	
-	private void displayObject() {
-		
-		final List <PositionStatisticBean>psbList = new ArrayList<PositionStatisticBean>();
-		psbList.addAll(symbolOpMap.values());		
-		
-		log.info("psbList:{}",psbList.size());
+
+	private void scheduleJob(AsyncTimerEvent timerEvent,
+			long maxRefreshInterval) {
+		Business.getInstance().getScheduleManager().scheduleRepeatTimerEvent(maxRefreshInterval,
+				PositionStatisticView.this, timerEvent);
+	}
+
+	private void cancelScheduleJob(AsyncTimerEvent timerEvent) {
+		Business.getInstance().getScheduleManager().cancelTimerEvent(timerEvent);		
+	}
+
+	public void showDialog(final String msg){
 		openPositionViewer.getControl().getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				synchronized(openPositionViewer) {
-					if(null == PositionStatisticView.this.symbolOpMap || openPositionViewer.isViewClosing())
-						return;
-					
-					if(PositionStatisticView.this.symbolOpMap.size() > 0)
-						createOpenPositionColumns(psbList);
-					
-					openPositionViewer.setInput(PositionStatisticView.this.symbolOpMap);
-
-					openPositionViewer.refresh();
-				}
+				MessageDialog.openWarning(openPositionViewer.getControl().getShell(), "Open Position Statistic", msg);
 			}
+		
 		});
-	}
-	
-	private boolean openPositionColumnsCreated = false;
-	private void createOpenPositionColumns(List  positions) {
-		if(!openPositionColumnsCreated) {
-			Object obj = positions.get(0);
-			List<ColumnProperty> properties = openPositionViewer.setObjectColumnProperties(obj);
-			
-			String[] from = {"PnL"};
-			String[] to = {"UrP&L"};
-			openPositionViewer.setTitleMap(from, to);
-			openPositionViewer.setSmartColumnProperties(obj.getClass().getName(), properties);
-			openPositionViewer.setInput(positions);
-			openPositionColumnsCreated = true;
-		}
 	}
 }
