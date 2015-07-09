7ce82202bee9964061bf5b3db762a8dab6d8c0f7
elvis <elvis.wu@hkfdt.com>
2015-07-09 15:39:31 +0800

    Update refDataFactory

--- common/src/main/java/com/cyanspring/common/staticdata/RefDataFactory.java
+++ common/src/main/java/com/cyanspring/common/staticdata/RefDataFactory.java
@@ -19,32 +19,32 @@ import com.cyanspring.common.staticdata.fu.MappingData;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
-public class RefDataFactory extends RefDataService{
-    
+public class RefDataFactory extends RefDataService {
+
     List<RefData> refDataList;
     private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-	private String market = Default.getMarket();
-	private XStream xstream = new XStream(new DomDriver("UTF-8"));
-	private Map<String,IRefDataStrategy> strategyMap = new HashMap<>();
+    private String market = Default.getMarket();
+    private XStream xstream = new XStream(new DomDriver("UTF-8"));
+    private Map<String, IRefDataStrategy> strategyMap = new HashMap<>();
     private Map<String, List<MappingData>> strategyMapping;
-	private MarketSessionUtil marketSessionUtil;
+    private MarketSessionUtil marketSessionUtil;
     private String strategyPack = "com.cyanspring.common.staticdata.fu";
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void init() throws Exception {
-		log.info("initialising with " + refDataFile);
-		File file = new File(refDataFile);
-		if (file.exists()) {
-            refDataList = (List<RefData>)xstream.fromXML(file);
-		} else {
-			throw new Exception("Missing refdata file: " + refDataFile);
-		}
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public void init() throws Exception {
+        log.info("initialising with " + refDataFile);
+        File file = new File(refDataFile);
+        if (file.exists()) {
+            refDataList = (List<RefData>) xstream.fromXML(file);
+        } else {
+            throw new Exception("Missing refdata file: " + refDataFile);
+        }
+    }
 
     @Override
     public boolean update(String tradeDate) throws Exception {
-        if (refDataList == null){
+        if (refDataList == null) {
             log.warn(this.getClass().getSimpleName() + "is not initial or initialising.");
             return false;
         }
@@ -52,16 +52,13 @@ public class RefDataFactory extends RefDataService{
         Calendar cal = Calendar.getInstance();
         cal.setTime(sdf.parse(tradeDate));
         IRefDataStrategy strategy;
-        for(RefData refData: refDataList) {
-            if(!strategyMap.containsKey(refData.getStrategy())){
+        for (RefData refData : refDataList) {
+            if (!strategyMap.containsKey(refData.getStrategy())) {
                 try {
-                    Class<IRefDataStrategy> tempClz = (Class<IRefDataStrategy>)Class.forName( strategyPack + "." + refData.getStrategy() + "Strategy");
+                    Class<IRefDataStrategy> tempClz = (Class<IRefDataStrategy>) Class.forName(strategyPack + "." + refData.getStrategy() + "Strategy");
                     Constructor<IRefDataStrategy> ctor = tempClz.getConstructor();
                     strategy = ctor.newInstance();
-                    List<MappingData> list = null;
-                    if (strategyMapping != null)
-                        list = strategyMapping.get(refData.getStrategy());
-
+                    List<MappingData> list = strategyMapping.get(refData.getStrategy());
                     strategy.setRequireData(marketSessionUtil, list);
                 } catch (Exception e) {
                     log.error("Can't find strategy: {}", refData.getStrategy());
@@ -84,7 +81,7 @@ public class RefDataFactory extends RefDataService{
                 }
                 strategyMap.put(refData.getStrategy(), strategy);
                 updateMarginRate(refData);
-            }else{
+            } else {
                 strategy = strategyMap.get(refData.getStrategy());
             }
             strategy.init(cal);
@@ -95,41 +92,41 @@ public class RefDataFactory extends RefDataService{
     }
 
     @Override
-	public void uninit() {
+    public void uninit() {
         log.info("uninitialising");
         strategyMap.clear();
-	}
-	
-	@Override
-	public RefData getRefData(String symbol) {
-        for (RefData refData : refDataList){
+    }
+
+    @Override
+    public RefData getRefData(String symbol) {
+        for (RefData refData : refDataList) {
             if (refData.getSymbol().equals(symbol))
                 return refData;
         }
         return null;
-	}
+    }
 
-	@Override
-	public List<RefData> getRefDataList() {
-		return refDataList;
-	}
+    @Override
+    public List<RefData> getRefDataList() {
+        return refDataList;
+    }
 
-	private void saveRefDataToFile(String path, List<RefData> list){
-		File file = new File(path);
-		try {
-			file.createNewFile();
-			FileOutputStream os = new FileOutputStream(file);
-			OutputStreamWriter writer = new OutputStreamWriter(os, Charset.forName("UTF-8"));
-			xstream.toXML(list, writer);
-			os.close();
-		} catch (IOException e) {
-			log.error(e.getMessage(), e);
-		}	
-	}
-	
-	public void setMarketSessionUtil(MarketSessionUtil marketSessionUtil) {
-		this.marketSessionUtil = marketSessionUtil;
-	}
+    private void saveRefDataToFile(String path, List<RefData> list) {
+        File file = new File(path);
+        try {
+            file.createNewFile();
+            FileOutputStream os = new FileOutputStream(file);
+            OutputStreamWriter writer = new OutputStreamWriter(os, Charset.forName("UTF-8"));
+            xstream.toXML(list, writer);
+            os.close();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public void setMarketSessionUtil(MarketSessionUtil marketSessionUtil) {
+        this.marketSessionUtil = marketSessionUtil;
+    }
 
     public void setStrategyPack(String strategyPack) {
         this.strategyPack = strategyPack;
