af7f00eb4195dab5671f6981a6ff216aae7f450b
jimmy <jimmy>
2015-07-21 13:16:33 +0800

    modify LiveTrading DayTradingMode clear order and position to configurable

--- common/src/main/java/com/cyanspring/common/event/livetrading/LiveTradingEndEvent.java
+++ common/src/main/java/com/cyanspring/common/event/livetrading/LiveTradingEndEvent.java
@@ -3,11 +3,15 @@ package com.cyanspring.common.event.livetrading;
 import com.cyanspring.common.event.RemoteAsyncEvent;
 
 public class LiveTradingEndEvent extends RemoteAsyncEvent{
-
-	public LiveTradingEndEvent(String key, String receiver) {
-		
+	
+	private boolean isNeedClearOrderPosition;
+	
+	public LiveTradingEndEvent(String key, String receiver,boolean isNeedClearOrderPostion) {
 		super(key, receiver);
-
+		this.isNeedClearOrderPosition = isNeedClearOrderPostion;
 	}
-
+	
+	public boolean isNeedClearOrderPosition() {
+		return isNeedClearOrderPosition;
+	}
 }
