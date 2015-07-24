62b18034db318e8d1c8cce6d5dc8eba7befbc181
calvin <calvin@hkfdt.com>
2015-07-24 16:10:12 +0800

    try Unit WindGateway

--- adaptor/src/main/java/com/cyanspring/adaptor/future/wind/gateway/WindGateway.java
+++ adaptor/src/main/java/com/cyanspring/adaptor/future/wind/gateway/WindGateway.java
@@ -1133,8 +1133,8 @@ public class WindGateway implements Runnable {
 		String OS = System.getProperty("os.name").toLowerCase();
 		if (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 ) {
 			log.info("OS : " + OS);
-			System.loadLibrary("libWHNetWork");
-			System.loadLibrary("libTDBAPI_v2");
+			System.load("libWHNetWork.so");
+			System.load("libTDBAPI_v2.so");
 		}
 		
 		String current = null;
