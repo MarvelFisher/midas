ed2cd8ef0405efd22fe5f533546f1dcba046d7a2
shuwei.kuo <shuwei.kuo@hkfdt.com>
2015-07-27 19:10:22 +0800

    modify WindRefDataAdapter connect mysql

--- adaptor/src/main/java/com/cyanspring/adaptor/future/wind/data/WindBaseDBData.java
+++ adaptor/src/main/java/com/cyanspring/adaptor/future/wind/data/WindBaseDBData.java
@@ -2,6 +2,9 @@ package com.cyanspring.adaptor.future.wind.data;
 
 import com.cyanspring.common.business.RefDataField;
 import com.cyanspring.common.data.DataObject;
+import com.cyanspring.common.marketdata.QuoteExtDataField;
+
+import java.util.Date;
 
 public class WindBaseDBData extends DataObject {
 
@@ -14,24 +17,43 @@ public class WindBaseDBData extends DataObject {
     public String getTWDisplayName() {
         return this.get(String.class, RefDataField.TW_DISPLAYNAME.value());
     }
-
     public void setTWDisplayName(String twName) {
         this.set(twName, RefDataField.TW_DISPLAYNAME.value());
     }
-
     public String getCNDisplayName() {
         return this.get(String.class, RefDataField.CN_DISPLAYNAME.value());
     }
-
     public void setCNDisplayName(String cnName) {
         this.set(cnName, RefDataField.CN_DISPLAYNAME.value());
     }
-
     public String getSpellName() {
         return this.get(String.class, RefDataField.SPELL_NAME.value());
     }
-
     public void setSpellName(String spellName) {
         this.set(spellName, RefDataField.SPELL_NAME.value());
+    }
+    public long getFreeShares() {
+        return this.get(long.class, QuoteExtDataField.FREESHARES.value());
+    }
+    public void setFreeShares(long freeShares) {
+        this.set(freeShares, QuoteExtDataField.FREESHARES.value());
+    }
+    public long getTotalShares() {
+        return this.get(long.class, QuoteExtDataField.TOTOALSHARES.value());
+    }
+    public void setTotalShares(long totalShares) {
+        this.set(totalShares, QuoteExtDataField.TOTOALSHARES.value());
+    }
+    public double getPERatio() {
+        return this.get(double.class, QuoteExtDataField.PERATIO.value());
+    }
+    public void setPERatio(double peRatio) {
+        this.set(peRatio, QuoteExtDataField.PERATIO.value());
+    }
+    public Date getTimeStamp(){
+        return this.get(Date.class, QuoteExtDataField.TIMESTAMP.value());
+    }
+    public void setTimeStamp(Date timeStamp){
+        this.set(timeStamp, QuoteExtDataField.TIMESTAMP.value());
     }
 }
