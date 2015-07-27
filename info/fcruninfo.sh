ad76401e4872dc0e3cc232f9d77d85eb4ff4696b
steve <steve@hkfdt.com>
2015-07-27 14:42:01 +0800

    fcruninfo set mem 2G

--- info/fcruninfo.sh
+++ info/fcruninfo.sh
@@ -8,7 +8,7 @@ fi
 
 sleep 5
 
-JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -Xloggc:log/info_gc`date +%Y%m%d_%H%M%S`.log -server -d64 -Xmx1g -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=0 -XX:MaxDirectMemorySize=1G -XX:AutoBoxCacheMax=3000000 -XX:MaxPermSize=128m"
+JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -Xloggc:log/info_gc`date +%Y%m%d_%H%M%S`.log -server -d64 -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=0 -XX:MaxDirectMemorySize=2G -XX:AutoBoxCacheMax=3000000 -XX:MaxPermSize=128m"
 echo "Starting LTS-INFO "`date` | tee -a ./log/console.log
 java ${JAVA_OPTS} -Duser.timezone=GMT+8 -jar jars/cyanspring-info-2.56.jar conf/info_fcserver.xml >> ./log/console.log &
 echo $! > LTS-INFO_PID
