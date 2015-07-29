cea947f502daf355e8b7e261755f583554efd992
shuwei.kuo <shuwei.kuo@hkfdt.com>
2015-07-29 10:21:49 +0800

    Modify ALL quotecleaner method

--- common/src/main/java/com/cyanspring/common/marketdata/ForexQuoteCleaner.java
+++ common/src/main/java/com/cyanspring/common/marketdata/ForexQuoteCleaner.java
@@ -13,9 +13,13 @@ public class ForexQuoteCleaner implements IQuoteCleaner{
         quote.setAskVol(0);
         quote.setBid(0);
         quote.setBidVol(0);
+        quote.setOpen(0);
+        quote.setHigh(0);
+        quote.setLow(0);
+        quote.setLast(0);
+        quote.setLastVol(0);
         quote.setTurnover(0);
         quote.setTotalVolume(0);
-        quote.setLast(0);
         quote.setTimeStamp(Clock.getInstance().now());
         if(null != quote.getBids() && quote.getBids().size() > 0) quote.getBids().clear();
         if(null != quote.getAsks() && quote.getAsks().size() > 0) quote.getAsks().clear();
