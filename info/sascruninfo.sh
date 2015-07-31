0e87d97140b557edf3ed26ed793559313f05f674
steve <steve@hkfdt.com>
2015-07-31 10:01:26 +0800

    runinfo disable jmxremote

--- info/sascruninfo.sh
+++ info/sascruninfo.sh
@@ -8,7 +8,7 @@ fi
 
 sleep 5
 
-JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=10.0.0.52 -Dcom.sun.management.jmxremote.port=1239 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+#JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=10.0.0.52 -Dcom.sun.management.jmxremote.port=1239 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -Xloggc:log/info_gc`date +%Y%m%d_%H%M%S`.log -server -d64 -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=0 -XX:MaxDirectMemorySize=2G -XX:AutoBoxCacheMax=3000000 -XX:MaxPermSize=128m"
 echo "Starting LTS-INFO "`date` | tee -a ./log/console.log
 java ${JAVA_OPTS} -Duser.timezone=GMT+8 -jar InfoServer.jar conf/info_sa_scserver.xml >> ./log/console.log &
