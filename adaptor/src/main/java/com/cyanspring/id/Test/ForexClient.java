524e63151d4e439012a8b0a86fb32ab9bd0e9658
steve <steve@hkfdt.com>
2015-08-07 17:02:02 +0800

    IDQuotesFeed version number

--- adaptor/src/main/java/com/cyanspring/id/Test/ForexClient.java
+++ adaptor/src/main/java/com/cyanspring/id/Test/ForexClient.java
@@ -24,7 +24,7 @@ import com.cyanspring.id.Library.Util.LogUtil;
 public class ForexClient implements IMarketDataListener,
 		IMarketDataStateListener, IFrameClose, AutoCloseable {
 	
-	public static final String _ClientName = "QuotesFeed 1.20150423.1";
+	public static final String _ClientName = "QuotesFeed 1.20150807.1";
 	private static final Logger log = LoggerFactory.getLogger(ForexClient.class);
 	
 	public static ForexClient instance = null;
