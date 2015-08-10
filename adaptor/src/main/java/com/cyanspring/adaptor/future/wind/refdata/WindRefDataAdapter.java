ade11747fb1ca11ef8603cb567bc775b547479f1
shuwei.kuo <shuwei.kuo@hkfdt.com>
2015-08-10 20:28:12 +0800

    Modify WindRefDataAdapter read file

--- adaptor/src/main/java/com/cyanspring/adaptor/future/wind/refdata/WindRefDataAdapter.java
+++ adaptor/src/main/java/com/cyanspring/adaptor/future/wind/refdata/WindRefDataAdapter.java
@@ -468,7 +468,7 @@ public class WindRefDataAdapter implements IRefDataAdaptor, IReqThreadCallback {
         } else {
             log.debug("get RefData from RefDataFile = " + refDataFile);
             List<RefData> refDataList = getListFromFile(refDataFile);
-            if(refDataList != null) listener.onRefData(refDataList);
+            listener.onRefData(refDataList);
         }
     }
 
