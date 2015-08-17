5a0ec316c420f424586c8a90bc211215f0716ef5
elvis <elvis.wu@hkfdt.com>
2015-08-17 13:47:45 +0800

    Add RefDataField comment

--- common/src/main/java/com/cyanspring/common/business/RefDataField.java
+++ common/src/main/java/com/cyanspring/common/business/RefDataField.java
@@ -27,35 +27,35 @@ public enum RefDataField {
 	CATEGORY("Category"),      // 商品分類 . ex IF
 	TICK_TABLE("Tick table"),  // tick table ref
 	SETTLEMENT_DATE("Settlement date"),  // 到期日
-	PRICE_PER_UNIT("Price per unit"),
+	PRICE_PER_UNIT("Price per unit"),    // 報價單位
 	EN_TRADING_UNIT("ENTrading unit"),   // 交易單位
 	TW_TRADING_UNIT("TWTrading unit"),   // 交易單位
 	CN_TRADING_UNIT("CNTrading unit"),   // 交易單位
 	MARGIN_RATE("Margin Rate"),          // 保證金 %%
-	COMMISSION_FEE("Commission fee"),    // 手續費
+	COMMISSION_FEE("Commission fee"),    // 百分比手續費
 	MINIMAL_COMMISSION_FEE("Minimal CF"),// 最低收續費
-	LOT_COMMISSION_FEE("Lot CF"),
+	LOT_COMMISSION_FEE("Lot CF"),        // 單手手續費
 	//MAXIMUM_LOT("Maximum lot"),     // 最大下單手數
-	LIMIT_MAXIMUM_LOT("Limit maximum lot"),
-	MARKET_MAXIMUM_LOT("Market maximum lot"), 
-	MAXIMUM_HOLD("Maximum hold"),
+	LIMIT_MAXIMUM_LOT("Limit maximum lot"),  // 現價單最大數量
+	MARKET_MAXIMUM_LOT("Market maximum lot"), // 市價單最大數量
+	MAXIMUM_HOLD("Maximum hold"),	// 最大持倉量
 	PRICE_LIMIT("Price limit"),     // 帳跌停板幅度 %
 	DECIMALPOINT("Decimal point"),  // 小數位數	
 	DENOMINATOR("Denominator"),     // 分母
 	NUMERATOR_DP("Nnumerator DP"),   // 分子小數位數 
-	TRADABLE("Tradable"),
+	TRADABLE("Tradable"),			// 是否可交易
 	STRATEGY("Strategy"),
 	SPELL_NAME("Spell name"),
-	COMMODITY("Commodity"),
+	COMMODITY("Commodity"),			// LTS 商品類型
 	DETAIL_EN("Detail EN"),
 	DETAIL_TW("Detail TW"),
 	DETAIL_CN("Detail CN"),
 	SPOT_ENNAME("Spot ENName"),
 	SPOT_CNNAME("Spot CNName"),
 	SPOT_TWNAME("Spot TWName"),
-	CODE("Code"),
-	ITYPE("IType"),
-	INDEX_SESSION_TYPE("Index Session Type")
+	CODE("Code"),							// WindCode
+	ITYPE("IType"),							// Wind商品類型
+	INDEX_SESSION_TYPE("Index Session Type")  // marketSessionType settlement/spot/exchange
 	;
 	
 	static HashMap<String, RefDataField> map = new HashMap<String, RefDataField>();
