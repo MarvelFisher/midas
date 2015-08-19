9844a4fef1725243e8359fd59cfb00c4c07579a0
elvis <elvis.wu@hkfdt.com>
2015-08-19 17:38:06 +0800

    Add unimplement method

--- common/src/main/java/com/cyanspring/common/staticdata/StockRefDataManager.java
+++ common/src/main/java/com/cyanspring/common/staticdata/StockRefDataManager.java
@@ -230,4 +230,16 @@ public class StockRefDataManager extends RefDataService {
 		strategy.init(cal,template);
 		strategy.updateRefData(refData);
 	}
+
+	@Override
+	public List<RefData> update(String index, String tradeDate) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(RefData refData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
