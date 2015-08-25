b3e0c46ea5b4d30d89c5d257a160b1b73471c7c9
steve <steve@hkfdt.com>
2015-08-25 10:55:03 +0800

    [PJ-37]CentralDbReadyEvent add indexList

--- common/src/main/java/com/cyanspring/common/event/info/CentralDbSubscribeEvent.java
+++ common/src/main/java/com/cyanspring/common/event/info/CentralDbSubscribeEvent.java
@@ -1,12 +1,23 @@
 package com.cyanspring.common.event.info;
 
+import java.util.List;
+
 import com.cyanspring.common.event.RemoteAsyncEvent;
 
 @SuppressWarnings("serial")
 public class CentralDbSubscribeEvent extends RemoteAsyncEvent 
 {
+	private List<String> indexList = null;
 	public CentralDbSubscribeEvent(String key, String receiver) 
 	{
 		super(key, receiver);
+	}
+	public List<String> getIndexList()
+	{
+		return indexList;
+	}
+	public void setIndexList(List<String> indexList)
+	{
+		this.indexList = indexList;
 	}
 }
