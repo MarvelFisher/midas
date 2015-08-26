1447fd952a69001923561fd92a6d9b124e015cf1
Claudia Zhou <claudia.zhou@hkfdt.com>
2015-08-26 18:02:53 +0800

    Add clientId to CoinSetting events.

--- common/src/main/java/com/cyanspring/common/event/account/CoinSettingReplyEvent.java
+++ common/src/main/java/com/cyanspring/common/event/account/CoinSettingReplyEvent.java
@@ -7,23 +7,29 @@ public class CoinSettingReplyEvent extends RemoteAsyncEvent {
     private static final long serialVersionUID = 1L;
     private String userId;
     private String txId;
+	private String clientId;
     private boolean isOk;
     private String message;
 
-    public CoinSettingReplyEvent(String key, String receiver, String userId, String txId, boolean isOk, String message) {
+    public CoinSettingReplyEvent(String key, String receiver, String userId, String txId, String clientId, boolean isOk, String message) {
         super(key, receiver);
         this.txId = txId;
         this.userId = userId;
+        this.clientId = clientId;
         this.isOk = isOk;
         this.message = message;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 
     public String getTxId() {
         return txId;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getClientId() {
+        return clientId;
     }
 
     public boolean isOk() {
@@ -34,4 +40,14 @@ public class CoinSettingReplyEvent extends RemoteAsyncEvent {
         return message;
     }
 
+    @Override
+    public String toString() {
+        return "CoinSettingReplyEvent{" +
+                "userId='" + userId + '\'' +
+                ", txId='" + txId + '\'' +
+                ", clientId='" + clientId + '\'' +
+                ", isOk=" + isOk +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
