e862192aa4caa7bdbb03e1f33dbb87c6d08b4366
jimmy <jimmy>
2015-08-26 15:50:22 +0800

    [PJ13]add DCEStrategy

---
+++ common/src/main/java/com/cyanspring/common/staticdata/fu/DCEStrategy.java
@@ -0,0 +1,64 @@
+package com.cyanspring.common.staticdata.fu;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import com.cyanspring.common.staticdata.RefData;
+import com.cyanspring.common.staticdata.RefDataException;
+
+public class DCEStrategy extends AbstractRefDataStrategy {
+	
+	protected static final Logger log = LoggerFactory.getLogger(DCEStrategy.class);
+	
+	@Override
+	public void init(Calendar cal, RefData template) {
+		super.init(cal, template);
+	}
+
+    @Override
+    public void updateRefData(RefData refData) {
+		
+		try {
+		
+			if( null == getTradeDateManager(refData.getCategory())){
+				log.info("refData:{} - tradeDateManager is null",refData.getCNDisplayName());
+				return;
+			}
+			
+			setTemplateData(refData);
+			String combineCnName = refData.getCNDisplayName();		
+			refData.setSettlementDate(calSettlementDate(refData.getSymbol(),refData.getCategory(),getContractDate(combineCnName),10));		
+			refData.setIndexSessionType(getIndexSessionType(refData));
+		} catch (RefDataException e){
+			log.warn(e.getMessage());
+		} catch (Exception e) {
+			log.error(e.getMessage(),e);
+		}
+    }
+
+	private String calSettlementDate(String symbol,String category,Calendar cal,int dayInMonth){
+		
+		cal.set(Calendar.DATE, cal.getMinimum(Calendar.DATE));
+		Date date = cal.getTime();
+		if( null == getTradeDateManager(category)){
+			log.warn("symbol:{} can't find tradeDateManager!",symbol);
+			return "";
+		}
+		
+		date = getTradeDateManager(category).preTradeDate(date);
+		for(int i=0 ; i < dayInMonth ; i++){
+			date = getTradeDateManager(category).nextTradeDate(date);
+		}
+		
+		return getSettlementDateFormat().format(date);
+	}
+    
+    @Override
+    public void setRequireData(Object... objects) {
+    	super.setRequireData(objects);
+    }
+}
