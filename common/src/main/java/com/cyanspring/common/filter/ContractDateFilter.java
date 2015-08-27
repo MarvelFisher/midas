1d30df6e9224a2b1b91024a51705f651e99a78da
alvin <alvin.xie@hkfdt.com>
2015-08-27 09:54:15 +0800

    Correct the filter name.

--- common/src/main/java/com/cyanspring/common/filter/ContractDateFilter.java
+++ common/src/main/java/com/cyanspring/common/filter/ContractDateFilter.java
@@ -2,7 +2,6 @@ package com.cyanspring.common.filter;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
@@ -13,11 +12,12 @@ import org.springframework.util.StringUtils;
 
 import com.cyanspring.common.staticdata.RefData;
 
-public class ContractDataFilter implements IRefDataFilter {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(ContractDataFilter.class);
+public class ContractDateFilter implements IRefDataFilter {
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ContractDateFilter.class);
 	private boolean checkValidContractDate;
-	
+
 	public boolean isCheckValidContractDate() {
 		return checkValidContractDate;
 	}
@@ -26,6 +26,17 @@ public class ContractDataFilter implements IRefDataFilter {
 		this.checkValidContractDate = checkValidContractDate;
 	}
 
+	/**
+	 * Check if the configured SettlementDate is valid
+	 * If SettlementDate is past time or more than 5 year to the future, filter it
+	 * 
+	 * Reference path:
+	 * server/conf/fc/fc.xml
+	 * 
+	 * @param lstRefData
+	 *            The RefData list to be filtered
+	 * @return The filtered RefData list
+	 */
 	@Override
 	public List<RefData> filter(List<RefData> lstRefData) throws Exception {
 		if (lstRefData != null && lstRefData.size() > 0) {
@@ -37,39 +48,42 @@ public class ContractDataFilter implements IRefDataFilter {
 			}
 		} else {
 			LOGGER.error("The given RefData list cannot be null or empty");
-			throw new Exception("The given RefData list cannot be null or empty");
+			throw new Exception(
+					"The given RefData list cannot be null or empty");
 		}
-		
+
 		return lstRefData;
 	}
-	
-	private boolean isValidContractDate(RefData refData){		
-		if( null == refData)
+
+	private boolean isValidContractDate(RefData refData) {
+		if (null == refData)
 			return false;
-		
-		String settlementDate	= null;	
+
+		String settlementDate = null;
 		try {
 			settlementDate = refData.getSettlementDate();
-			if(!StringUtils.hasText(settlementDate))
+			if (!StringUtils.hasText(settlementDate))
 				return false;
-			
+
 			SimpleDateFormat contractFormat = new SimpleDateFormat("yyyy-MM-dd");
 			Calendar now = Calendar.getInstance();
-			int thisYear = now.get(Calendar.YEAR);			
+			int thisYear = now.get(Calendar.YEAR);
 			Calendar contractCal = Calendar.getInstance();
 			contractCal.setTime(contractFormat.parse(settlementDate));
 			int contractYear = contractCal.get(Calendar.YEAR);
 
-			if((contractYear-thisYear)>=5 || (contractYear-thisYear)<0){
+			if ((contractYear - thisYear) >= 5 || (contractYear - thisYear) < 0) {
 				return false;
-			}else{
+			} else {
 				return true;
 			}
-					
+
 		} catch (ParseException e) {
-			LOGGER.warn("not valid contract date:{},{}",settlementDate,e.getMessage());
-		} catch (Exception e){
-			LOGGER.warn("can't find settlementDate :{},{}",refData.getSymbol(),e.getMessage());
+			LOGGER.warn("not valid contract date:{},{}", settlementDate,
+					e.getMessage());
+		} catch (Exception e) {
+			LOGGER.warn("can't find settlementDate :{},{}",
+					refData.getSymbol(), e.getMessage());
 		}
 
 		return false;
