df9a7a1d89f036b479377bf195b48bc6e1dfea79
alvin <alvin.xie@hkfdt.com>
2015-10-01 14:24:23 +0800

    Update LTFT commission fee calculation
    - Re-formulate the calculation of FT commission fee
    - Update template_FT.xml and set LotCF of FITX to 80
    - Update template_FT.xml and set LotCF of FIMTX to 20
    - Rename class FITXCommissionManager to FTCommissionManager and update related referrences

---
+++ server/src/main/java/com/cyanspring/server/account/FTCommissionManager.java
@@ -0,0 +1,39 @@
+package com.cyanspring.server.account;
+
+import webcurve.util.PriceUtils;
+
+import com.cyanspring.common.account.AccountSetting;
+import com.cyanspring.common.business.Execution;
+import com.cyanspring.common.staticdata.RefData;
+
+public class FTCommissionManager extends CommissionManager {
+
+	private double trxTaxRate = 0.00002;
+
+	public double getTrxTaxRate() {
+		return trxTaxRate;
+	}
+
+	public void setTrxTaxRate(double trxTaxRate) {
+		this.trxTaxRate = trxTaxRate;
+	}
+
+	@Override
+	public double getCommission(RefData refData, AccountSetting settings, double value, Execution execution) {
+		// Currently FT only set Lot CF, call super to get Lot CF (already applied accountCommission)
+		double lotCF = super.getCommission(refData, settings, value, execution);
+
+		double accountCommission = 1;
+		if (settings != null && !PriceUtils.isZero(settings.getCommission())) {
+			accountCommission = settings.getCommission();
+		}
+
+        double tax = value * trxTaxRate;
+        tax = Math.round(tax) * accountCommission;
+
+        double commissionFee = tax + lotCF;
+
+        return commissionFee;
+	}
+
+}
