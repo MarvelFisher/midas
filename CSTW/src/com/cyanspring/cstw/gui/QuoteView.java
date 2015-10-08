670764045495c867ee2e9272699eceab36f1593e
jimmy <jimmy>
2015-10-08 11:14:08 +0800

    [PT-64]fix default quantity renew info

--- CSTW/src/com/cyanspring/cstw/gui/QuoteView.java
+++ CSTW/src/com/cyanspring/cstw/gui/QuoteView.java
@@ -235,15 +235,12 @@ public class QuoteView extends ViewPart implements IAsyncEventListener {
 		if (event instanceof QuoteEvent) {
 			
 			QuoteEvent e = (QuoteEvent) event;
-//			if (!PriceUtils.isZero(e.getQuote().getAsk())
-//					&& !PriceUtils.isZero(e.getQuote().getBid())) {
-				
-				quoteMap.put(e.getQuote().getSymbol(), e.getQuote());
-				if(e.getKey().equals(receiverId)) { // first time receive, immediately refresh
-					refreshQuote();
-				}
-				updated = true;
-//			}
+
+			quoteMap.put(e.getQuote().getSymbol(), e.getQuote());
+			if(e.getKey().equals(receiverId)) { // first time receive, immediately refresh
+				refreshQuote();
+			}
+			updated = true;
 		} else if (event instanceof AsyncTimerEvent) {
 			if(updated)
 				refreshQuote();
@@ -290,8 +287,6 @@ public class QuoteView extends ViewPart implements IAsyncEventListener {
 								.getName(), properties);
 
 						columnCreated = true;
-//						quoteViewer.setSorter(new QuoteSorter());
-//						quoteViewer.getTable().setSortColumn(quoteViewer.getTable().getColumn(symbolColNum));
 						quoteViewer.setInput(list);
 						
 					} 
