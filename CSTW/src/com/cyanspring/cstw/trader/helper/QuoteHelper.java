8268fc80cb4e36e7aebe1922010da711577083cb
xiaofeng <ningxiaofeng@hkfdt.cn>
2015-10-14 20:37:29 +0800

    [PT-76]

---
+++ CSTW/src/com/cyanspring/cstw/trader/helper/QuoteHelper.java
@@ -0,0 +1,19 @@
+package com.cyanspring.cstw.trader.helper;
+
+import com.cyanspring.common.marketdata.Quote;
+import com.cyanspring.common.util.PriceUtils;
+
+public class QuoteHelper {
+
+	public static boolean checkValid(Quote quote) {
+		if (PriceUtils.Equal(quote.getAsk(), 0)
+				&& (PriceUtils.Equal(quote.getBid(), 0))
+				&& (PriceUtils.Equal(quote.getAskVol(), 0))
+				&& (PriceUtils.Equal(quote.getBidVol(), 0))
+				&& (PriceUtils.Equal(quote.getHigh(), 0))
+				&& (PriceUtils.Equal(quote.getLow(), 0))) {
+			return false;
+		}
+		return true;
+	}
+}
