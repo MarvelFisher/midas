dd9157e428b890c7d587343bba9bab9c5b2cdc4e
Claudia Zhou <claudia.zhou@hkfdt.com>
2015-10-20 10:07:04 +0800

    [PJ-212] Change MI server memory from 3G to 1G.

--- server/mirun.sh
+++ server/mirun.sh
@@ -16,7 +16,7 @@ sleep 5
 export LD_LIBRARY_PATH=$WORKDIR/windlib:$LD_LIBRARY_PATH
 
 #JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=$ip -Dcom.sun.management.jmxremote.port=1234 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -Xloggc:log/gc`date +%Y%m%d_%H%M%S`.log -server -d64 -Xmx3g -Xms3g -XX:PermSize=256m -XX:+ParallelRefProcEnabled -XX:+PrintAdaptiveSizePolicy -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -XX:MaxDirectMemorySize=1G -XX:AutoBoxCacheMax=3000000"
+JAVA_OPTS="${JAVA_OPTS} -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -Xloggc:log/gc`date +%Y%m%d_%H%M%S`.log -server -d64 -Xmx1g -Xms1g -XX:PermSize=256m -XX:+ParallelRefProcEnabled -XX:+PrintAdaptiveSizePolicy -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=100 -XX:+UseG1GC -XX:MaxDirectMemorySize=1G -XX:AutoBoxCacheMax=3000000"
 echo "Starting LTS "`date` | tee -a ./log/console.log
 java ${JAVA_OPTS} -Duser.timezone=GMT+8 -cp jars/cyanspring-server-2.56.jar com.cyanspring.server.MarketDataServer conf/miserver.xml conf/common/mylog4j.xml >> ./log/console.log &
 echo $! > LTS_PID
