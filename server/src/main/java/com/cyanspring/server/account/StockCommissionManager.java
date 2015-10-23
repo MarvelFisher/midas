b1f0b3651474aed81c9c2c833a80d070ebaa123b
alvin <alvin.xie@hkfdt.com>
2015-10-23 17:54:21 +0800

    Fix bug in calculation commission fee based on iType and leverage on IType.isStock()

--- server/src/main/java/com/cyanspring/server/account/StockCommissionManager.java
+++ server/src/main/java/com/cyanspring/server/account/StockCommissionManager.java
@@ -26,9 +26,7 @@ public class StockCommissionManager extends CommissionManager {
     public double getCommission(RefData refData, AccountSetting settings, double value, Execution execution) {
     	if (refData != null) {
         	String type = refData.getIType();
-        	if (type != null && (!IType.STOCK.getValue().equals(type) 
-        			&& !IType.STOCK_GEI.getValue().equals(type)
-        			&& !IType.STOCK_SSE.getValue().equals(type))) {
+        	if (type != null && !IType.isStock(type)) {
 				return super.getCommission(refData, settings, value, execution);
 			}
         }
@@ -61,7 +59,7 @@ public class StockCommissionManager extends CommissionManager {
     public double getCommission(RefData refData, AccountSetting settings, Execution execution) {
     	if (refData != null) {
         	String type = refData.getIType();
-        	if (type != null && (IType.FUTURES_CX.getValue().equals(type) || IType.FUTURES_IDX.getValue().equals(type))) {
+        	if (type != null && !IType.isStock(type)) {
 				return super.getCommission(refData, settings, execution);
 			}
         }
