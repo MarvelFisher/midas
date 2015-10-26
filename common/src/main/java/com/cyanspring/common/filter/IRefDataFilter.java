2c10f98dc0a636baf53a183f7786685c8f831e56
elvis <elvis.wu@hkfdt.com>
2015-10-26 11:12:38 +0800

    [PJ-254] update available position validator and delete useless files

--- common/src/main/java/com/cyanspring/common/filter/IRefDataFilter.java
+++
@@ -1,9 +0,0 @@
-package com.cyanspring.common.filter;
-
-import com.cyanspring.common.staticdata.RefData;
-
-import java.util.List;
-
-public interface IRefDataFilter {
-	List<RefData> filter(List<RefData> lstRefData) throws Exception;
-}
