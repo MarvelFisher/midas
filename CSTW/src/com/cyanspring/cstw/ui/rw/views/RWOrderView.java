3ee0141c0e59a6452119ed3a8bd667e92d9619d6
yujunfeng <yujunfeng@hkfdt.cn>
2015-10-26 15:14:46 +0800

    [PT-79] add RW Order View and menu

---
+++ CSTW/src/com/cyanspring/cstw/ui/rw/views/RWOrderView.java
@@ -0,0 +1,30 @@
+package com.cyanspring.cstw.ui.rw.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * @author Junfeng
+ * @create 26 Oct 2015
+ */
+public class RWOrderView extends ViewPart {
+	public static String ID = "com.cyanspring.cstw.ui.rw.views.RWOrderView";
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+	 */
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
