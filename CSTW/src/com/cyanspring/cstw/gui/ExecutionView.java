64f1a128850f950bf738e6d1e2f1acedb4f5d5a7
jimmy <jimmy>
2015-10-28 17:36:47 +0800

    [PT-116]modify ExecutionView

--- CSTW/src/com/cyanspring/cstw/gui/ExecutionView.java
+++ CSTW/src/com/cyanspring/cstw/gui/ExecutionView.java
@@ -15,31 +15,34 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.part.ViewPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import com.cyanspring.common.BeanHolder;
+import com.cyanspring.common.account.UserGroup;
 import com.cyanspring.common.business.Execution;
-import com.cyanspring.common.business.OrderField;
 import com.cyanspring.common.event.AsyncEvent;
 import com.cyanspring.common.event.IAsyncEventListener;
+import com.cyanspring.common.event.order.AllExecutionSnapshotReplyEvent;
+import com.cyanspring.common.event.order.AllExecutionSnapshotRequestEvent;
 import com.cyanspring.common.event.order.ChildOrderUpdateEvent;
-import com.cyanspring.common.event.order.ExecutionSnapshotEvent;
-import com.cyanspring.common.event.order.ExecutionSnapshotRequestEvent;
 import com.cyanspring.cstw.business.Business;
-import com.cyanspring.cstw.event.InstrumentSelectionEvent;
-import com.cyanspring.cstw.event.MultiInstrumentStrategySelectionEvent;
-import com.cyanspring.cstw.event.ObjectSelectionEvent;
-import com.cyanspring.cstw.event.SingleInstrumentStrategySelectionEvent;
-import com.cyanspring.cstw.event.SingleOrderStrategySelectionEvent;
+import com.cyanspring.cstw.common.ImageID;
+import com.cyanspring.cstw.event.AccountSelectionEvent;
 import com.cyanspring.cstw.gui.common.ColumnProperty;
 import com.cyanspring.cstw.gui.common.DynamicTableViewer;
+import com.cyanspring.cstw.gui.common.StyledAction;
+import com.cyanspring.cstw.gui.filter.ParentOrderFilter;
 
 public class ExecutionView extends ViewPart implements IAsyncEventListener {
 	private static final Logger log = LoggerFactory
@@ -47,20 +50,22 @@ public class ExecutionView extends ViewPart implements IAsyncEventListener {
 	
 	public static final String ID = "com.cyanspring.cstw.gui.ExecutionView";
 	private DynamicTableViewer viewer;
-	private String objectId;
-	private String currentParentId;
-	@SuppressWarnings("unused")
-	private String currentSymbol;
-	@SuppressWarnings("unused")
-	private boolean isSingleOrderStrategy;
-	private boolean columnSet;
-
+	private boolean columnSet;	
+	private Action pinAction;
+	private boolean pinned;
+	private String TOOLBAR_ID_PIN="TOOLBAR_PIN";
+	private ParentOrderFilter accountFilter;
+	private ImageRegistry imageRegistry;
+	private String currentAccount;
+	private List<Execution> executionList = new ArrayList<Execution>();
+	
 	public ExecutionView() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
+		imageRegistry = Activator.getDefault().getImageRegistry();
+
 		// create parent layout
 		GridLayout layout = new GridLayout(1, false);
 		parent.setLayout(layout);
@@ -79,132 +84,165 @@ public class ExecutionView extends ViewPart implements IAsyncEventListener {
 		gridData.horizontalAlignment = GridData.FILL;
 		viewer.getControl().setLayoutData(gridData);
 
+		// create pin order action
+		createPinAction(parent);
+		
 		// subscribe to business event
-		Business.getInstance().getEventManager().subscribe(SingleOrderStrategySelectionEvent.class, this);
-		Business.getInstance().getEventManager().subscribe(SingleInstrumentStrategySelectionEvent.class, this);
-		Business.getInstance().getEventManager().subscribe(ExecutionSnapshotEvent.class, this);
 		Business.getInstance().getEventManager().subscribe(ChildOrderUpdateEvent.class, this);
-		Business.getInstance().getEventManager().subscribe(MultiInstrumentStrategySelectionEvent.class, this);
-		Business.getInstance().getEventManager().subscribe(InstrumentSelectionEvent.class, this);
+		Business.getInstance().getEventManager().subscribe(AllExecutionSnapshotReplyEvent.class, this);
+		Business.getInstance().getEventManager().subscribe(AccountSelectionEvent.class, this);
+		
+		UserGroup ug = Business.getInstance().getUserGroup();
+		List<String> id = Business.getInstance().getAccountGroup();
+		if(ug.isAdmin()){
+			id = null;
+		}else if(!ug.getRole().isManagerLevel()){
+			id = new ArrayList<String>();
+			id.add(Business.getInstance().getAccount());
+		}
+		
+		AllExecutionSnapshotRequestEvent event = new AllExecutionSnapshotRequestEvent(ID,Business.getInstance().getFirstServer(),id);
+		try {
+			Business.getInstance().getEventManager().sendRemoteEvent(event);
+		} catch (Exception e) {
+			log.warn(e.getMessage(),e);
+		}
+		
+		if(!StringUtils.hasText(currentAccount)){
+			currentAccount = Business.getInstance().getAccount();
+		}
+	}
+
+	private void createPinAction(Composite parent) {
+		pinAction = new StyledAction("",
+				org.eclipse.jface.action.IAction.AS_CHECK_BOX) {
+			public void run() {
+				pinned = pinned ? false : true;
+				if (!pinned) {
+					viewer.removeFilter(accountFilter);
+				} else {
+					accountFilter.setMatch("Account", Business.getInstance()
+							.getAccount());
+					viewer.addFilter(accountFilter);
+				}
+			}
+		};
+
+		pinAction.setId(TOOLBAR_ID_PIN);
+		pinAction.setChecked(true);
+		pinned = true;
+		accountFilter = new ParentOrderFilter();
+		accountFilter.setMatch("Account", Business.getInstance().getAccount());
+		viewer.addFilter(accountFilter);
+
+		pinAction.setText("Pin Account");
+		pinAction.setToolTipText("Pin account");
+
+		ImageDescriptor imageDesc = imageRegistry.getDescriptor(ImageID.PIN_ICON.toString());
+		pinAction.setImageDescriptor(imageDesc);
+
+		IActionBars bars = getViewSite().getActionBars();
+		bars.getToolBarManager().add(pinAction);		
 	}
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void showSnapshot(ExecutionSnapshotEvent event) {
-		List<Execution> executions = event.getExecutions();
-		ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
-		if(executions != null)
-			for(Execution exec: executions) {
-				if(currentParentId != null && !currentParentId.equals(exec.getParentOrderId()))
-					continue;
-				data.add(exec.getFields());
-			}
-		show(data);
 	}
 	
-	private void show(ArrayList<Map<String, Object>> data) {
+	private List<Map<String, Object>> executionListToMap(List<Execution> list){
+		
+		if(null == list)
+			return null;
+		
+		List<Map<String, Object>> tempMapList = new ArrayList<Map<String, Object>>();
+		for(Execution exe : list){
+			tempMapList.add(exe.getFields());
+		}
+		
+		return tempMapList;
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		Business.getInstance().getEventManager().unsubscribe(ChildOrderUpdateEvent.class, this);
+		Business.getInstance().getEventManager().unsubscribe(AllExecutionSnapshotReplyEvent.class, this);
+		Business.getInstance().getEventManager().unsubscribe(AccountSelectionEvent.class, this);
+	}
+	
+	private void show(final List<Map<String, Object>> data) {
 		if (data == null)
 			return;
 		
-		if (!columnSet && data.size()>0) {
-			Map<String, Object> map = data.get(0);
-			ArrayList<ColumnProperty> columnProperties = new ArrayList<ColumnProperty>();
-			Set<String> titles = map.keySet();
-			for(String title: titles)
-				columnProperties.add(new ColumnProperty(title, 100));
+		if(viewer.getControl().getDisplay().isDisposed())
+			return;
+		
+		viewer.getControl().getDisplay().asyncExec(new Runnable(){
+
+			@Override
+			public void run() {
+				if (!columnSet && data.size()>0) {
+					Map<String, Object> map = data.get(0);
+					ArrayList<ColumnProperty> columnProperties = new ArrayList<ColumnProperty>();
+					Set<String> titles = map.keySet();
+					for(String title: titles)
+						columnProperties.add(new ColumnProperty(title, 100));
+					
+					viewer.setSmartColumnProperties(viewer.getTableLayoutFile(), columnProperties);
+					columnSet = true;
+					viewer.setInput(data);
+				}
+				if(viewer.getInput() == null || viewer.getInput() != data)
+					viewer.setInput(data);
+				viewer.refresh();
+			}
 			
-			viewer.setSmartColumnProperties(viewer.getTableLayoutFile(), columnProperties);
-			columnSet = true;
-			viewer.setInput(data);
-		}
-		if(viewer.getInput() == null || viewer.getInput() != data)
-			viewer.setInput(data);
-		viewer.refresh();
+		});
+
 	}
 
-	private void updateExecution(Execution execution) {
-		if(objectId == null || !objectId.equals(execution.getStrategyId()) || 
-				   ((currentParentId != null) && !currentParentId.equals(execution.getParentOrderId())))
-					return;
-				
-		@SuppressWarnings("unchecked")
-		final ArrayList<Map<String, Object>> data = null == viewer.getInput()?
-				new ArrayList<Map<String, Object>>() : (ArrayList<Map<String, Object>>)viewer.getInput();
-		boolean found = false;
-		for(int i=0; i<data.size(); i++) {
-			Map<String, Object> map = data.get(i);
-			String id = (String)map.get(OrderField.ID.value());
-			if(id.equals(execution.getId())) {
-				data.set(i, execution.getFields());
-				found = true;
-				break;
-			}
-		}
-		if(!found)
-			data.add(execution.getFields());
+	synchronized private void updateExecution(Execution execution) {
+		if(null == execution)
+			return;
 		
+		executionList.add(execution);		
 		viewer.getControl().getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				show(data);
+				show(executionListToMap(executionList));
 			}
 		});
 	}
 
 	@Override
 	public void onEvent(final AsyncEvent e) {
-		if (e instanceof ObjectSelectionEvent) {
-			ObjectSelectionEvent event = (ObjectSelectionEvent)e;
-			Map<String, Object> map = event.getData();
-			objectId = (String)map.get(OrderField.ID.value());
-			String server = (String)map.get(OrderField.SERVER_ID.value());
-			if(e instanceof InstrumentSelectionEvent) {
-				objectId = (String)map.get(OrderField.STRATEGY_ID.value());
-				currentParentId = (String)map.get(OrderField.ID.value());
-				currentSymbol = (String)map.get(OrderField.SYMBOL.value());
-				isSingleOrderStrategy = false;
-			} else if (e instanceof MultiInstrumentStrategySelectionEvent) {
-				currentParentId = null;
-				currentSymbol = null;
-				isSingleOrderStrategy = false;
-			} else if(e instanceof SingleOrderStrategySelectionEvent  ||
-					  e instanceof SingleInstrumentStrategySelectionEvent) {
-				currentSymbol = (String)map.get(OrderField.SYMBOL.value());
-				currentParentId = null;
-				isSingleOrderStrategy = true;
-			}
-			
-			ExecutionSnapshotRequestEvent request = new ExecutionSnapshotRequestEvent(objectId, server);
-			try {
-				Business.getInstance().getEventManager().sendRemoteEvent(request);
-			} catch (Exception ex) {
-				log.error(ex.getMessage(), ex);
-				ex.printStackTrace();
-			}
-
-		} else if (e instanceof ExecutionSnapshotEvent) {
-			viewer.getControl().getDisplay().asyncExec(new Runnable() {
-				@Override
-				public void run() {
-					showSnapshot((ExecutionSnapshotEvent)e);
-					final Table table = viewer.getTable();
-					if(table.getItemCount()>0)
-						table.setSelection(0);
-				}
-			});
-			
-		} else if (e instanceof ChildOrderUpdateEvent) {
+		if (e instanceof ChildOrderUpdateEvent) {
+			log.info("get ChildOrderUpdateEvent");
 			ChildOrderUpdateEvent event = (ChildOrderUpdateEvent)e;
 			Execution execution = event.getExecution();
 			if(event.getExecution() == null)
 				return;
-			
+			else{
+				log.info("execution is null");
+			}
 			updateExecution(execution);
-		} else {
+		} else if (e instanceof AllExecutionSnapshotReplyEvent) {
+			AllExecutionSnapshotReplyEvent event = (AllExecutionSnapshotReplyEvent)e;
+			if(!event.isOk()){
+				log.warn("process AllExecutionSnapshotReplyEvent fail:{}",event.getMessage());
+				return;
+			}
+			List<Execution> list = event.getExecutionList();
+			executionList = list;
+			log.info("get AllExecution list:{}",list.size());
+			show(executionListToMap(list));
+		}else if (e instanceof AccountSelectionEvent) {
+			currentAccount = ((AccountSelectionEvent) e).getAccount();
+			if (pinned) {
+				accountFilter.setMatch("Account", currentAccount);
+				show(executionListToMap(executionList));
+			}			
+		}else{
 			log.error("Unhandled event: " + e.getClass());
 		}
 	}
