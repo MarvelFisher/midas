c1115c9cbd286e6048f0ccec42adbd56e83ca469
alvin <alvin.xie@hkfdt.com>
2015-11-04 09:54:34 +0800

    Add boolean 'liveTrading' and set to enable recover ChildOrder in LiveTrading only

--- server/src/main/java/com/cyanspring/server/StrategyRecoveryProcessor.java
+++ server/src/main/java/com/cyanspring/server/StrategyRecoveryProcessor.java
@@ -11,18 +11,22 @@ import com.cyanspring.common.type.PersistType;
 import com.cyanspring.server.persistence.PersistenceManager;
 
 public class StrategyRecoveryProcessor implements IRecoveryProcessor<DataObject> {
+
 	@Autowired
 	private PersistenceManager persistenceManager;
 
-	private boolean todayOnly;
+	private boolean todayOnly = false;
+	private boolean liveTrading = false;
 
 	@Override
 	public List<DataObject> recover() {
-		List<DataObject> list = new ArrayList<DataObject>();
+		List<DataObject> list = new ArrayList<>();
 		list.addAll(persistenceManager.recoverObject(PersistType.SINGLE_ORDER_STRATEGY, todayOnly));
 		list.addAll(persistenceManager.recoverObject(PersistType.SINGLE_INSTRUMENT_STRATEGY, todayOnly));
 		list.addAll(persistenceManager.recoverObject(PersistType.MULTI_INSTRUMENT_STRATEGY, todayOnly));
-		list.addAll(persistenceManager.recoverChildOrderAudit(todayOnly));
+		if (liveTrading) {
+			list.addAll(persistenceManager.recoverChildOrderAudit(todayOnly));
+		}
 		return list;
 	}
 
@@ -33,4 +37,13 @@ public class StrategyRecoveryProcessor implements IRecoveryProcessor<DataObject>
 	public void setTodayOnly(boolean todayOnly) {
 		this.todayOnly = todayOnly;
 	}
+
+	public boolean isLiveTrading() {
+		return liveTrading;
+	}
+
+	public void setLiveTrading(boolean liveTrading) {
+		this.liveTrading = liveTrading;
+	}
+
 }
