fad3848053b7f8338088521be8bb56a4b8a3ea2a
xiaofeng <ningxiaofeng@hkfdt.cn>
2015-11-09 10:18:20 +0800

    [PJ-135]增加交易员，管理员两个模块的缓存

---
+++ CSTW/src/com/cyanspring/cstw/session/AdminSession.java
@@ -0,0 +1,15 @@
+package com.cyanspring.cstw.session;
+
+/**
+ * 
+ * 
+ * Admin 包含原先RiskControl 以及 BW两个功能。
+ * 
+ * @author NingXiaofeng
+ * 
+ * @date 2015.11.09
+ *
+ */
+public final class AdminSession {
+
+}
---
+++ CSTW/src/com/cyanspring/cstw/session/TraderSession.java
@@ -0,0 +1,12 @@
+package com.cyanspring.cstw.session;
+
+/**
+ * 
+ * @author NingXiaofeng
+ * 
+ * @date 2015.11.09
+ *
+ */
+public final class TraderSession {
+
+}
