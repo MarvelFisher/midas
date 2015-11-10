59b3dba97c20664fe685b83ba0ce156ea739c9ac
jimmy <jimmy>
2015-11-10 09:49:11 +0800

    [PT-140]add shortkey

--- server/src/main/java/com/cyanspring/server/validation/FcRefDataChecker.java
+++ server/src/main/java/com/cyanspring/server/validation/FcRefDataChecker.java
@@ -2,13 +2,13 @@ package com.cyanspring.server.validation;
 
 import com.cyanspring.common.staticdata.IRefDataChecker;
 import com.cyanspring.common.staticdata.RefData;
-import com.cyanspring.common.staticdata.fu.IType;
+import com.cyanspring.common.staticdata.RefDataBitUtil;
 
 public class FcRefDataChecker implements IRefDataChecker {
 
 	@Override
 	public boolean check(RefData refData) {
-		if(IType.isFuture(refData.getIType()))
+		if(RefDataBitUtil.isFutures(refData.getInstrumentType()))
 			return true;
 		
 		return false;
