78fb612be45ab7f53000198c3605c5c709dd5afa
alvin <alvin.xie@hkfdt.com>
2015-11-18 10:55:30 +0800

    Add BU, Index, Odds, TX ContractPolicyTest

---
+++ common/src/test/java/com/cyanspring/common/staticdata/policy/IndexContractPolicyTest.java
@@ -0,0 +1,51 @@
+package com.cyanspring.common.staticdata.policy;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import mockit.Mock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cyanspring.common.staticdata.RefData;
+
+public class IndexContractPolicyTest {
+
+	RefData data;
+	Calendar cal;
+
+	@Before
+	public void Before() {
+		data = new RefData();
+		cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, 2016);
+		cal.set(Calendar.MONTH, 0);
+		cal.set(Calendar.DATE, 1);
+	}
+
+	@Test
+	public void test() {
+		IndexContractPolicy policy = new IndexContractPolicy() {
+			@Mock
+			public Calendar getFirstContractMonth(RefData refData) {
+				return cal;
+			}
+		};
+
+		List<String> lstExpectedContractMonth = new ArrayList<String>() {{
+			add("201601");
+			add("201602");
+			add("201603");
+			add("201606");
+		}};
+
+		List<String> lstContractMonth = policy.getContractMonths(data);
+		lstExpectedContractMonth.removeAll(lstContractMonth);
+		assertTrue(lstExpectedContractMonth.size() == 0);
+	}
+
+}
