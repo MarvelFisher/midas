62696dda373da5734354f7fea2d6e9031e9eea1b
dennis <dennis.chen@hkfdt.com>
2015-11-23 11:16:14 +0800

    [PJ-347] add account and symbol to MaxOrderQtyAllowedReplyEvent

--- common/src/main/java/com/cyanspring/common/event/account/MaxOrderQtyAllowedReplyEvent.java
+++ common/src/main/java/com/cyanspring/common/event/account/MaxOrderQtyAllowedReplyEvent.java
@@ -4,15 +4,27 @@ import com.cyanspring.common.event.AbstractReplyEvent;
 
 public class MaxOrderQtyAllowedReplyEvent extends AbstractReplyEvent {
 	private static final long serialVersionUID = 1L;
+	private String account;
+	private String symbol;
 	private double qty;
 	private double cashAvailable;
 
 	public MaxOrderQtyAllowedReplyEvent(String key, String receiver,
-			boolean ok, String txId, String msg, double qty,
-			double cashAvailable) {
+			boolean ok, String txId, String msg, String account, String symbol,
+			double qty, double cashAvailable) {
 		super(key, receiver, ok, txId, msg);
+		this.account = account;
+		this.symbol = symbol;
 		this.qty = qty;
 		this.cashAvailable = cashAvailable;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public String getSymbol() {
+		return symbol;
 	}
 
 	public double getQty() {
