938691ad5f00aa3a5a5962083d949878ad98acc1
yujunfeng <yujunfeng@hkfdt.cn>
2015-11-24 17:59:31 +0800

    [PJ-307] SubPool Management frame

---
+++ CSTW/src/com/cyanspring/cstw/service/iservice/riskmgr/ISubPoolManageService.java
@@ -0,0 +1,11 @@
+package com.cyanspring.cstw.service.iservice.riskmgr;
+
+import com.cyanspring.cstw.service.iservice.IBasicService;
+
+/**
+ * @author Junfeng
+ * @create 24 Nov 2015
+ */
+public interface ISubPoolManageService extends IBasicService{
+
+}
