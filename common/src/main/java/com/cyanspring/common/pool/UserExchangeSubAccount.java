0a9e9f014ceda02d64797f0778d33ac6aca3f302
David <guowei@hkfdt.cn>
2015-11-24 14:55:09 +0800

    【303】Assign User to ExchangeSubAccount

--- common/src/main/java/com/cyanspring/common/pool/UserExchangeSubAccount.java
+++ common/src/main/java/com/cyanspring/common/pool/UserExchangeSubAccount.java
@@ -6,16 +6,16 @@ public class UserExchangeSubAccount implements Serializable {
 
 	private static final long serialVersionUID = -395487495460995664L;
 
-	private String account;
+	private String user;
 
 	private String exchangeSubAccount;
 
-	public String getAccount() {
-		return account;
+	public String getUser() {
+		return user;
 	}
 
-	public void setAccount(String account) {
-		this.account = account;
+	public void setUser(String user) {
+		this.user = user;
 	}
 
 	public String getExchangeSubAccount() {
